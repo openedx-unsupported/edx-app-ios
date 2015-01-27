a92b549beec3fc1c04a841fbea8a521772a24185
Akiva Leffert <akiva@edx.org>
2015-01-26 23:34:21 -0500

    Remove unused code

--- edXVideoLocker/OEXAppDelegate.m
+++ edXVideoLocker/OEXAppDelegate.m
@@ -34,9 +34,6 @@ typedef void (^completionHandler)();
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    UILocalNotification* localNotification = [[UILocalNotification alloc] init];
-    localNotification.applicationIconBadgeNumber = 0;
-    
     // Segment IO initialization
     // If you want to see debug logs from inside the SDK.
     
@@ -125,33 +122,6 @@ typedef void (^completionHandler)();
         return NO;
 }
 
-
-
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-//    [self.locationManager startUpdatingLocation];
-
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
-    UILocalNotification* localNotification = [[UILocalNotification alloc] init];
-    localNotification.applicationIconBadgeNumber = 0;
-}
-
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
@@ -168,12 +138,6 @@ typedef void (^completionHandler)();
     _isSocialMediaLogin=NO;
     _handleFacebookSchema=NO;
     _handleGoogleSchema=NO;
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-
 }
 
 - (BOOL)application: (UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation{
