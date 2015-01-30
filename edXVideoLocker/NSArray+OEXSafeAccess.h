45788d844f7e082f3873c572fbf8a670b76456df
Akiva Leffert <akiva@edx.org>
2015-01-30 16:24:22 -0500

    Remove assertion if part of the path is missing.
    We're expecting some courses to have overly short paths. This was
    causing a crash on some courses when doing a DEBUG build. Release builds
    were fine.

--- edXVideoLocker/NSArray+OEXSafeAccess.h
+++ edXVideoLocker/NSArray+OEXSafeAccess.h
@@ -11,8 +11,13 @@
 @interface NSArray (OEXSafeGetAccess)
 
 /// Like objectAtIndex: but return nil instead of crashing if the index is out of bounds
-/// Will still assert if the index is out of bounds in DEBUG builds.
+/// Will still assert if the index is out of bounds in DEBUG builds. See oex_objectOrNilAtIndex:
+/// if you want to ignore the nil case entirely.
 - (id)oex_safeObjectAtIndex:(NSUInteger)index;
+
+/// Like objectAtIndex: but return nil instead of crashing if the index is out of bounds
+/// See oex_objectOrNilAtIndex: if you want fail fast behavior for fetching out of bounds
+- (id)oex_safeObjectOrNilAtIndex:(NSUInteger)index;
 
 @end
 
@@ -21,10 +26,10 @@
 
 
 /// Like addObject: but won't crash if object is nil. Will still assert on DEBUG builds
-/// See safeAddObjectOrNil: if you want to ignore the nil case entirely
+/// See oex_addObjectOrNil: if you want to ignore the nil case entirely
 - (void)oex_safeAddObject:(id)object;
 
-/// Like addObject: but won't crash if object is nil. See safeAddObject: if you want fail fast
+/// Like addObject: but won't crash if object is nil. See oex_safeAddObject: if you want fail fast
 /// behavior for setting nil
 - (void)oex_safeAddObjectOrNil:(id)object;
 
