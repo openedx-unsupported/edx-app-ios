846259832d2b3e51505833940e4459b211291c8e
abhra-clarice <abhradeep@abhradeeps-mbp.clarice.com>
2015-01-30 14:27:16 +0530

    Fix vertical appearance of Closed Caption - subtract constant number of pixels

--- edXVideoLocker/CutomePlayer/CLVideoPlayerControls.m
+++ edXVideoLocker/CutomePlayer/CLVideoPlayerControls.m
@@ -788,7 +788,7 @@ static const CGFloat iPhoneScreenPortraitWidth = 320.f;
         Y_offset = bottomOffset;
     }
     
-    CGSize size = [self.subtitleLabel sizeThatFits:CGSizeMake(self.bounds.size.width*0.97, CGFLOAT_MAX)];
+    CGSize size = [self.subtitleLabel sizeThatFits:CGSizeMake(self.bounds.size.width-40.0, CGFLOAT_MAX)];
     self.subtitleLabel.bounds = ({
         CGRect bounds = self.subtitleLabel.bounds;
         bounds.size = size;
