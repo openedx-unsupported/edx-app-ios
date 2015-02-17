c5815aa1c3fdd45bff691d17c2146bf3f7e2e1f5
abhra-clarice <abhradeep@abhradeeps-mbp.clarice.com>
2015-02-17 11:56:48 +0530

    Update test cases and add class for Enrollment notification message

---
+++ edXVideoLocker/OEXEnrollmentMessage.h
@@ -0,0 +1,13 @@
+//
+//  OEXEnrollmentMessage.h
+//  edXVideoLocker
+//
+//  Created by Abhradeep on 17/02/15.
+//  Copyright (c) 2015 edX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OEXEnrollmentMessage : NSObject
+
+@end
---
+++ edXVideoLocker/OEXEnrollmentMessage.m
@@ -0,0 +1,13 @@
+//
+//  OEXEnrollmentMessage.m
+//  edXVideoLocker
+//
+//  Created by Abhradeep on 17/02/15.
+//  Copyright (c) 2015 edX. All rights reserved.
+//
+
+#import "OEXEnrollmentMessage.h"
+
+@implementation OEXEnrollmentMessage
+
+@end
