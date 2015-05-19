8b2832179743121f23c75c88ea7068c2d39d1107
Tommy Ryan <tryan@qti.qualcomm.com>
2015-05-19 15:05:05 -0700

    Fixing temp oauth URL

--- Source/OEXNetworkConstants.h
+++ Source/OEXNetworkConstants.h
@@ -37,7 +37,7 @@
 #define URL_RESET_PASSWORD  @"/password_reset/"
 #define URL_SUBSTRING_VIDEOS @"edx-course-videos"
 #define URL_SUBSTRING_ASSETS @"asset/"
-#define AUTHORIZATION_URL @"/api/oauth2/access_token"
+#define AUTHORIZATION_URL @"/oauth2/access_token"
 #define URL_GET_USER_INFO @"/api/mobile/v0.5/my_user_info"
 // For Closed Captioning
 #define URL_VIDEO_SRT_FILE @"/api/mobile/v0.5/video_outlines/transcript/"
