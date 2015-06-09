630ea604c783b2c0c13472033949a157b3a14e0a
Ehmad Zubair Chughtai <ezubair@gmail.com>
2015-06-09 18:32:12 +0500

    minor code refactor

--- Source/OEXRegistrationViewController.m
+++ Source/OEXRegistrationViewController.m
@@ -323,12 +323,9 @@ NSString* const OEXExternalRegistrationWithExistingAccountNotification = @"OEXEx
     
     [self.registerButton setFrame:CGRectMake(horizontalSpacing, offset, contentWidth, 40)];
 
-    if ([self isRTL]) {
-        self.progressIndicator.center = CGPointMake(40, self.registerButton.frame.size.height / 2);
-    }
-    else {
-        self.progressIndicator.center = CGPointMake(self.registerButton.frame.size.width - 40, self.registerButton.frame.size.height / 2);
-    }
+    const int progressIndicatorCenterX = [self isRTL] ? 40 : self.registerButton.frame.size.width - 40;
+
+        self.progressIndicator.center = CGPointMake(progressIndicatorCenterX, self.registerButton.frame.size.height / 2);
     
     [self.scrollView addSubview:self.registerButton];
     offset = offset + 40;
