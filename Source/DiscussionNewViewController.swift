a9676eb8767d2609a2050d16c25e4ea0377b638b
Jeff Tang <jefftang@qti.qualcomm.com>
2015-06-18 19:11:12 -0700

    add response and comment UI - after PR: https://openedx.atlassian.net/browse/MQ-187

---
+++ Source/DiscussionNewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DiscussionNewViewController.swift
+//  edX
+//
+//  Created by Tang, Jeff on 6/18/15.
+//  Copyright (c) 2015 edX. All rights reserved.
+//
+
+import UIKit
+
+class DiscussionNewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Source/String+OEXFormatting.swift
@@ -0,0 +1,9 @@
+//
+//  String+OEXFormatting.swift
+//  edX
+//
+//  Created by Tang, Jeff on 6/18/15.
+//  Copyright (c) 2015 edX. All rights reserved.
+//
+
+import Foundation
