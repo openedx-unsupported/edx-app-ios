b3c08a76be6f4dac1589fcb50f26c61fa0cff4ee
Ehmad Zubair Chughtai <ezubair@gmail.com>
2015-07-27 18:50:53 +0500

    redoing the accessibilityLabels of buttons

--- Source/OEXFindCourseTableViewCell.m
+++ Source/OEXFindCourseTableViewCell.m
@@ -18,6 +18,8 @@
 }
 
 - (void)setAccessibilityLabels {
+    self.btn_DontSeeCourse.accessibilityLabel = self.btn_DontSeeCourse.titleLabel.text;
     self.btn_FindACourse.accessibilityHint = OEXLocalizedString(@"ACCESSIBILITY_OPENS_MOBILE_FRIENDLY_COURSES", nil);
+    self.btn_FindACourse.accessibilityLabel = self.btn_FindACourse.titleLabel.text;
 }
 @end
