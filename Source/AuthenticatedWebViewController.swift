2e5110591eacd4fd1866315aecf23ba66f94aa99
Akiva Leffert <akiva@edx.org>
2015-08-21 13:47:11 -0400

    Support for WKWebView in HTML blocks.
    Note: Does not actually enable said support while I investigate a web
    content issue related to view ports.
    
    See: https://openedx.atlassian.net/browse/MA-960
    
    JIRA: https://openedx.atlassian.net/browse/MA-790

--- Source/AuthenticatedWebViewController.swift
+++ Source/AuthenticatedWebViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import WebKit
 
 class HeaderViewInsets : ContentInsetsSource {
     weak var insetsDelegate : ContentInsetsSourceDelegate?
@@ -22,13 +23,96 @@ class HeaderViewInsets : ContentInsetsSource {
     }
 }
 
+private protocol WebContentController {
+    var view : UIView {get}
+    var scrollView : UIScrollView {get}
+    
+    var alwaysRequiresOAuthUpdate : Bool { get}
+    
+    var initialContentState : AuthenticatedWebViewController.State { get }
+    
+    func loadURLRequest(request : NSURLRequest)
+    
+    func clearDelegate()
+    func resetState()
+}
+
+private class UIWebViewContentController : WebContentController {
+    private let webView = UIWebView(frame: CGRectZero)
+    
+    var view : UIView {
+        return webView
+    }
+    
+    var scrollView : UIScrollView {
+        return webView.scrollView
+    }
+    
+    func clearDelegate() {
+        return webView.delegate = nil
+    }
+
+    func loadURLRequest(request: NSURLRequest) {
+        webView.loadRequest(request)
+    }
+    
+    func resetState() {
+        webView.stopLoading()
+        webView.loadHTMLString("", baseURL: nil)
+    }
+    
+    var alwaysRequiresOAuthUpdate : Bool {
+        return true
+    }
+    
+    var initialContentState : AuthenticatedWebViewController.State {
+        return AuthenticatedWebViewController.State.CreatingSession
+    }
+    
+    
+}
+
+private class WKWebViewContentController : WebContentController {
+    private let webView = WKWebView(frame: CGRectZero)
+    
+    var view : UIView {
+        return webView
+    }
+    
+    var scrollView : UIScrollView {
+        return webView.scrollView
+    }
+    
+    func clearDelegate() {
+        return webView.navigationDelegate = nil
+    }
+    
+    func loadURLRequest(request: NSURLRequest) {
+        webView.loadRequest(request)
+    }
+    
+    func resetState() {
+        webView.stopLoading()
+        webView.loadHTMLString("", baseURL: nil)
+    }
+    
+    var alwaysRequiresOAuthUpdate : Bool {
+        return false
+    }
+    
+    var initialContentState : AuthenticatedWebViewController.State {
+        return AuthenticatedWebViewController.State.LoadingContent
+    }
+}
+
 // Allows access to course content that requires authentication.
 // Forwarding our oauth token to the server so we can get a web based cookie
-public class AuthenticatedWebViewController: UIViewController, UIWebViewDelegate, UIScrollViewDelegate {
+public class AuthenticatedWebViewController: UIViewController, UIWebViewDelegate, UIScrollViewDelegate, WKNavigationDelegate {
     
     private enum State {
         case CreatingSession
         case LoadingContent
+        case NeedingSession
     }
     
     public struct Environment {
@@ -44,10 +128,26 @@ public class AuthenticatedWebViewController: UIViewController, UIWebViewDelegate
     }
     
     private let environment : Environment
-    private var webView : UIWebView?
     private let loadController : LoadStateViewController
     private let insetsController : ContentInsetsController
     private let headerInsets : HeaderViewInsets
+    
+    // After we drop support for iOS7, we can just always use WKWebView
+    private lazy var webController : WebContentController = {
+        // Temporarily disable the WKWebView version while we figure out why we're not sending a
+        // a meta name="viewport" tag. See https://openedx.atlassian.net/browse/MA-960
+//        if NSClassFromString("WKWebView") != nil {
+//            let controller = WKWebViewContentController()
+//            controller.webView.navigationDelegate = self
+//            return controller
+//        }
+//        else {
+            let controller = UIWebViewContentController()
+            controller.webView.delegate = self
+            return controller
+//        }
+        
+    }()
     
     private var state = State.CreatingSession
     
@@ -70,24 +170,26 @@ public class AuthenticatedWebViewController: UIViewController, UIWebViewDelegate
         fatalError("init(coder:) has not been implemented")
     }
     
+    deinit {
+        // Prevent crash due to stale back pointer, since WKWebView's UIScrollView apparently doesn't
+        // use weak for its delegate
+        webController.scrollView.delegate = nil
+        webController.clearDelegate()
+    }
+    
     override public func viewDidLoad() {
         
-        webView = {
-            let webView = UIWebView(frame: self.view.bounds)
-            webView.delegate = self
-            self.view.addSubview(webView)
-            webView.snp_makeConstraints({ (make) -> Void in
-                make.edges.equalTo(self.view)
-            })
-            self.loadController.setupInController(self, contentView: webView)
-            webView.scrollView.backgroundColor = self.environment.styles?.standardBackgroundColor()
-            webView.backgroundColor = self.environment.styles?.standardBackgroundColor()
-            webView.scrollView.delegate = self
-            
-            self.insetsController.setupInController(self, scrollView: webView.scrollView)
-            
-            return webView
-        }()
+        self.state = webController.initialContentState
+        self.view.addSubview(webController.view)
+        webController.view.snp_makeConstraints {make in
+            make.edges.equalTo(self.view)
+        }
+        self.loadController.setupInController(self, contentView: webController.view)
+        webController.view.backgroundColor = self.environment.styles?.standardBackgroundColor()
+        webController.scrollView.backgroundColor = self.environment.styles?.standardBackgroundColor()
+        webController.scrollView.delegate = self
+        
+        self.insetsController.setupInController(self, scrollView: webController.scrollView)
         
         
         if let request = self.contentRequest {
@@ -104,11 +206,32 @@ public class AuthenticatedWebViewController: UIViewController, UIWebViewDelegate
         super.didReceiveMemoryWarning()
         
         if view.window == nil {
-            webView?.stopLoading()
-            webView?.loadHTMLString("about:blank", baseURL: nil)
+            webController.resetState()
         }
         resetState()
     }
+    
+    
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        insetsController.updateInsets()
+    }
+    
+    public func showError(error : NSError?, icon : Icon? = nil, message : String? = nil) {
+        loadController.state = LoadState.failed(error : error, icon : icon, message : message)
+    }
+    
+    // MARK: Header View
+    
+    public func scrollViewDidScroll(scrollView: UIScrollView) {
+        // Ideally we'd just add the header view directly to the webview's .scrollview,
+        // and then we would get scrolling with the content for free
+        // but that totally screwed up leading/trailing constraints for a label
+        // inside a UIWebView, so we go this route instead
+        
+        headerInsets.view?.transform = CGAffineTransformMakeTranslation(0, -(scrollView.contentInset.top + scrollView.contentOffset.y))
+    }
+
     
     var headerView : UIView? {
         get {
@@ -117,15 +240,15 @@ public class AuthenticatedWebViewController: UIViewController, UIWebViewDelegate
         set {
             headerInsets.view?.removeFromSuperview()
             headerInsets.view = newValue
-            if let webView = webView, headerView = newValue {
-                webView.addSubview(headerView)
+            if let headerView = newValue {
+                webController.view.addSubview(headerView)
                 headerView.snp_makeConstraints {make in
                     make.top.equalTo(self.snp_topLayoutGuideBottom)
-                    make.leading.equalTo(webView)
-                    make.trailing.equalTo(webView)
+                    make.leading.equalTo(webController.view)
+                    make.trailing.equalTo(webController.view)
                 }
-                webView.setNeedsLayout()
-                webView.layoutIfNeeded()
+                webController.view.setNeedsLayout()
+                webController.view.layoutIfNeeded()
             }
         }
     }
@@ -134,16 +257,7 @@ public class AuthenticatedWebViewController: UIViewController, UIWebViewDelegate
         return environment.config?.apiHostURL() ?? ""
     }
     
-    public func loadRequest(request : NSURLRequest) {
-        contentRequest = request
-        loadController.state = .Initial
-        state = .CreatingSession
-        
-        // First make sure we have a valid session token.
-        // We don't really want to do this on every screen - but it's tricky not to, because
-        // UIWebView doesn't send back HTTP status codes, so we can't tell if we're getting a 401 or 403
-        // and get a new token. When we drop iOS7 support we should switch to WKWebView which will let us
-        // actually examine the status code. See https://openedx.atlassian.net/browse/MA-790
+    private func loadOAuthRefreshRequest() {
         NSURL(string:apiHostURL + "/oauth2/login/").map { URL -> Void in
             let exchangeRequest = NSMutableURLRequest(URL: URL)
             exchangeRequest.HTTPMethod = HTTPMethod.POST.rawValue
@@ -151,14 +265,86 @@ public class AuthenticatedWebViewController: UIViewController, UIWebViewDelegate
             for (key, value) in self.environment.session?.authorizationHeaders ?? [:] {
                 exchangeRequest.addValue(value, forHTTPHeaderField: key)
             }
-            self.webView?.loadRequest(exchangeRequest)
+            self.webController.loadURLRequest(exchangeRequest)
         }
     }
     
-    public override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        insetsController.updateInsets()
+    // MARK: Request Loading
+    
+    public func loadRequest(request : NSURLRequest) {
+        contentRequest = request
+        loadController.state = .Initial
+        state = webController.initialContentState
+        
+        if webController.alwaysRequiresOAuthUpdate {
+            loadOAuthRefreshRequest()
+        }
+        else {
+            webController.loadURLRequest(request)
+        }
     }
+    
+    // MARK: WKWebView delegate
+    
+    
+    public func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+        switch navigationAction.navigationType {
+        case .LinkActivated, .FormSubmitted, .FormResubmitted:
+            navigationAction.request.URL.map { UIApplication.sharedApplication().openURL($0) }
+            decisionHandler(.Cancel)
+        default:
+            decisionHandler(.Allow)
+        }
+    }
+    
+    public func webView(webView: WKWebView, decidePolicyForNavigationResponse navigationResponse: WKNavigationResponse, decisionHandler: (WKNavigationResponsePolicy) -> Void) {
+        
+        let continueAction : () -> Void = {
+            self.state = .LoadingContent
+            decisionHandler(.Allow)
+        }
+        
+        if let httpResponse = navigationResponse.response as? NSHTTPURLResponse,
+            statusCode = OEXHTTPStatusCode(rawValue: httpResponse.statusCode)
+            where state == .LoadingContent
+        {
+            if statusCode.is4xx {
+                decisionHandler(.Cancel)
+                loadOAuthRefreshRequest()
+                state = .NeedingSession
+            }
+            else {
+                continueAction()
+            }
+        }
+        else {
+            continueAction()
+        }
+    }
+    
+    public func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        switch state {
+        case .CreatingSession:
+            if let request = contentRequest {
+                state = .LoadingContent
+                webController.loadURLRequest(request)
+            }
+            else {
+                loadController.state = LoadState.failed()
+            }
+        case .LoadingContent:
+            loadController.state = .Loaded
+        case .NeedingSession:
+            state = .CreatingSession
+            loadOAuthRefreshRequest()
+        }
+    }
+    
+    public func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
+        showError(error)
+    }
+    
+    // MARK: UIWebView delegate
     
     public func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         if navigationType == .LinkClicked || navigationType == .FormSubmitted {
@@ -173,32 +359,21 @@ public class AuthenticatedWebViewController: UIViewController, UIWebViewDelegate
         case .CreatingSession:
             if let request = contentRequest {
                 state = .LoadingContent
-                self.webView?.loadRequest(request)
+                webController.loadURLRequest(request)
             }
             else {
                 loadController.state = LoadState.failed()
             }
         case .LoadingContent:
             loadController.state = .Loaded
+        case .NeedingSession:
+            loadOAuthRefreshRequest()
+            state = .CreatingSession
         }
     }
     
     public func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
         showError(error)
     }
-    
-    public func showError(error : NSError?, icon : Icon? = nil, message : String? = nil) {
-        loadController.state = LoadState.failed(error : error, icon : icon, message : message)
-    }
-    
-    public func scrollViewDidScroll(scrollView: UIScrollView) {
-        // Ideally we'd just the header view directly to the webview's .scrollview,
-        // and then we would get scrolling with the content for free
-        // but that totally screwed up leading/trailing constraints for a label 
-        // inside it, so we go this route instead
-        
-        headerInsets.view?.transform = CGAffineTransformMakeTranslation(0, -(scrollView.contentInset.top + scrollView.contentOffset.y))
-    }
-
 }
 
