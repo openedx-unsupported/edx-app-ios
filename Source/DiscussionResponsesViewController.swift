f38d9c0e8e675a1a11bac58a79117f2ddbb8a652
Michael Katz <mikekatz@users.noreply.github.com>
2015-08-28 10:09:10 -0400

    fix typo

--- Source/DiscussionResponsesViewController.swift
+++ Source/DiscussionResponsesViewController.swift
@@ -67,7 +67,7 @@ public class PostViewModel {
     init(item : DiscussionPostItem) {
         // This bug is blocking the resizing : http://stackoverflow.com/questions/2282629/how-to-get-height-for-nsattributedstring-at-a-fixed-width
         self.title = NSAttributedString(string: item.title, attributes: titleStyle.attributes)
-        self.body = NSAttributedString(string: item.body, attributes: bodyStyle.attributes))
+        self.body = NSAttributedString(string: item.body, attributes: bodyStyle.attributes)
         self.title = titleStyle.attributedStringWithText(item.title)
         self.body = bodyStyle.attributedStringWithText(item.body)
     }
