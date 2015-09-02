0a7970d7cd46fd4d3ba7051477465c0a0929fbb9
Ehmad Zubair Chughtai <ezubair@gmail.com>
2015-09-02 12:02:30 +0500

    assorted Discussion improvements

--- Source/DiscussionCommentsViewController.swift
+++ Source/DiscussionCommentsViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 private var largeTextStyle : OEXTextStyle {
-    return OEXTextStyle(weight: .Normal, size: .Small, color : OEXStyles.sharedStyles().neutralDark())
-}
-
-private var mediaTextStyle : OEXTextStyle {
     return OEXTextStyle(weight: .Normal, size: .XSmall, color : OEXStyles.sharedStyles().neutralDark())
 }
 
+private var mediaTextStyle : OEXTextStyle {
+    return OEXTextStyle(weight: .Normal, size: .XXXSmall, color : OEXStyles.sharedStyles().neutralBase())
+}
+
 private var smallTextStyle : OEXTextStyle {
-    return OEXTextStyle(weight: .Normal, size: .XXXSmall, color : OEXStyles.sharedStyles().neutralDark())
+    return OEXTextStyle(weight: .Normal, size: .XXXSmall, color : OEXStyles.sharedStyles().neutralBase())
+}
+
+private var smallIconStyle : OEXTextStyle {
+    return OEXTextStyle(weight: .Normal, size: .XXXSmall, color: OEXStyles.sharedStyles().neutralDark())
 }
 
 class DiscussionCommentCell: UITableViewCell {
@@ -41,28 +45,25 @@ class DiscussionCommentCell: UITableViewCell {
         bodyTextLabel.snp_makeConstraints { (make) -> Void in
             make.leading.equalTo(contentView).offset(DiscussionCommentCell.marginX)
             make.trailing.equalTo(contentView).offset(-DiscussionCommentCell.marginX)
-            make.top.equalTo(contentView).offset(5)
+            make.top.equalTo(contentView).offset(10)
         }
         
         contentView.addSubview(authorLabel)
         authorLabel.snp_makeConstraints { (make) -> Void in
             make.leading.equalTo(bodyTextLabel)
-            make.width.equalTo(80)
-            make.top.equalTo(bodyTextLabel.snp_bottom).offset(5)
+            make.bottom.equalTo(contentView).offset(-10)
         }
         
         contentView.addSubview(dateTimeLabel)
         dateTimeLabel.snp_makeConstraints { (make) -> Void in
-            make.top.equalTo(bodyTextLabel.snp_bottom).offset(5)
-            make.width.equalTo(100)
+            make.centerY.equalTo(authorLabel)
             make.leading.equalTo(authorLabel.snp_trailing).offset(2)
         }
     
         contentView.addSubview(commentCountOrReportIconButton)
         commentCountOrReportIconButton.snp_makeConstraints { (make) -> Void in
-            make.trailing.equalTo(contentView).offset(-5)
-            make.width.equalTo(120)
-            make.top.equalTo(bodyTextLabel.snp_bottom)
+            make.trailing.equalTo(contentView).offset(-10)
+            make.centerY.equalTo(authorLabel)
         }
         
         self.divider.backgroundColor = OEXStyles.sharedStyles().neutralLight()
@@ -77,6 +78,22 @@ class DiscussionCommentCell: UITableViewCell {
         }
     }
     
+    func useResponse(response : DiscussionResponseItem) {
+        self.bodyTextLabel.attributedText = largeTextStyle.attributedStringWithText(response.body)
+        self.authorLabel.attributedText = smallTextStyle.attributedStringWithText(response.author)
+        self.dateTimeLabel.attributedText = smallTextStyle.attributedStringWithText(response.createdAt.timeAgoSinceNow())
+        
+        self.backgroundColor = OEXStyles.sharedStyles().neutralWhiteT()
+        
+        let buttonTitle = NSAttributedString.joinInNaturalLayout([
+            Icon.Comment.attributedTextWithStyle(smallIconStyle),
+            smallTextStyle.attributedStringWithText(NSString.oex_stringWithFormat(OEXLocalizedStringPlural("COMMENT", Float(response.commentCount), nil), parameters: ["count": Float(response.commentCount)]))])
+        self.commentCountOrReportIconButton.setAttributedTitle(buttonTitle, forState: .Normal)
+    }
+    
+    func useComment(comment : DiscussionComment) {
+        
+    }
     
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -84,15 +101,17 @@ class DiscussionCommentCell: UITableViewCell {
     
 }
 
-class DiscussionCommentsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class DiscussionCommentsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, StandardViewControllerUIMethods {
     
     class Environment {
         private var courseDataManager : CourseDataManager?
         private weak var router: OEXRouter?
+        private var networkManager : NetworkManager?
         
-        init(courseDataManager : CourseDataManager, router: OEXRouter?) {
+        init(courseDataManager : CourseDataManager, router: OEXRouter?, networkManager : NetworkManager) {
             self.courseDataManager = courseDataManager
             self.router = router
+            self.networkManager = networkManager
         }
     }
     
@@ -102,10 +121,10 @@ class DiscussionCommentsViewController: UIViewController, UITableViewDataSource,
     }
     
     private let identifierCommentCell = "CommentCell"
-    private let minBodyTextHeight: CGFloat = 70.0
-    private let nonBodyTextHeight: CGFloat = 40.0
-    private let defaultResponseCellHeight: CGFloat = 100.0
-    private let defaultCommentCellHeight: CGFloat = 90.0
+    private let minBodyTextHeight: CGFloat = 20.0
+    private let nonBodyTextHeight: CGFloat = 35.0
+    private let defaultResponseCellHeight: CGFloat = 50.0
+    private let defaultCommentCellHeight: CGFloat = 55.0
     
     private let environment: Environment
     private let courseID: String
@@ -130,9 +149,49 @@ class DiscussionCommentsViewController: UIViewController, UITableViewDataSource,
  
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView = UITableView(frame: view.bounds, style: .Plain)
+        tableView.registerClass(DiscussionCommentCell.classForCoder(), forCellReuseIdentifier: identifierCommentCell)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        setStyles()
+        addSubviews()
+        setConstraints()
+        
+        addCommentButton.oex_addAction({[weak self] (action : AnyObject!) -> Void in
+            if let owner = self {
+                owner.environment.router?.showDiscussionNewCommentFromController(owner, courseID: owner.courseID, item: DiscussionItem.Response(owner.responseItem))
+            }
+            }, forEvents: UIControlEvents.TouchUpInside)
+        
+        discussionManager?.commentAddedStream.listen(self) {[weak self] result in
+            result.ifSuccess {
+                self?.addedItem($0.threadID, item: $0.comment)
+            }
+        }
+        
+        self.comments = responseItem.children
+        self.tableView.reloadData()
+        
+    }
+    
+    func addSubviews() {
+        view.addSubview(addCommentButton)
+        view.addSubview(tableView)
+    }
+    
+    func setStyles() {
+        
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        tableView.applyStandardSeparatorInsets()
+        tableView.backgroundColor = OEXStyles.sharedStyles().neutralXLight()
+        tableView.contentInset = UIEdgeInsetsMake(10.0, 0, 0, 0)
+        tableView.layer.cornerRadius = OEXStyles.sharedStyles().boxCornerRadius()
+        tableView.clipsToBounds = true
+        
         self.navigationItem.title = OEXLocalizedString("COMMENTS", nil)
-        view.backgroundColor = OEXStyles.sharedStyles().standardBackgroundColor()
+        view.backgroundColor = OEXStyles.sharedStyles().neutralXLight()
         
         addCommentButton.backgroundColor = OEXStyles.sharedStyles().primaryXDarkColor()
         
@@ -141,13 +200,10 @@ class DiscussionCommentsViewController: UIViewController, UITableViewDataSource,
         addCommentButton.setAttributedTitle(buttonTitle, forState: .Normal)
         addCommentButton.contentVerticalAlignment = .Center
         
-        addCommentButton.oex_addAction({[weak self] (action : AnyObject!) -> Void in
-            if let owner = self {
-                owner.environment.router?.showDiscussionNewCommentFromController(owner, courseID: owner.courseID, item: DiscussionItem.Response(owner.responseItem))
-            }
-        }, forEvents: UIControlEvents.TouchUpInside)
-        
-        view.addSubview(addCommentButton)
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: " ", style: .Plain, target: nil, action: nil)
+    }
+    
+    func setConstraints() {
         addCommentButton.snp_makeConstraints{ (make) -> Void in
             make.leading.equalTo(view)
             make.trailing.equalTo(view)
@@ -155,33 +211,14 @@ class DiscussionCommentsViewController: UIViewController, UITableViewDataSource,
             make.bottom.equalTo(view.snp_bottom)
         }
         
-        tableView = UITableView(frame: view.bounds, style: .Plain)
-        tableView.separatorStyle = UITableViewCellSeparatorStyle.None
-        tableView.applyStandardSeparatorInsets()
-        
-        if let theTableView = tableView {
-            theTableView.registerClass(DiscussionCommentCell.classForCoder(), forCellReuseIdentifier: identifierCommentCell)
-            theTableView.dataSource = self
-            theTableView.delegate = self
-            view.addSubview(theTableView)
-        }
-        
         tableView.snp_makeConstraints { (make) -> Void in
-            make.leading.equalTo(view)
-            make.top.equalTo(view).offset(10)
-            make.trailing.equalTo(view)
+            make.leading.equalTo(view.snp_leadingMargin)
+            make.top.equalTo(view)
+            make.trailing.equalTo(view.snp_trailingMargin)
             make.bottom.equalTo(addCommentButton.snp_top)
         }
         
-        tableView.reloadData()
         
-        discussionManager?.commentAddedStream.listen(self) {[weak self] result in
-            result.ifSuccess {
-                self?.addedItem($0.threadID, item: $0.comment)
-            }
-        }
-
-        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: " ", style: .Plain, target: nil, action: nil)
     }
     
     func addedItem(threadID: String, item: DiscussionComment) {
@@ -198,7 +235,8 @@ class DiscussionCommentsViewController: UIViewController, UITableViewDataSource,
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         switch TableSection(rawValue : indexPath.section) {
         case .Some(.Response):
-            return defaultResponseCellHeight
+            let resizeableContentHeight = UITableViewCell.heightForLabelWithAttributedText(largeTextStyle.attributedStringWithText(responseItem.body), cellWidth: tableView.frame.size.width - 2.0 * DiscussionCommentCell.marginX)
+            return defaultResponseCellHeight + resizeableContentHeight
         case .Some(.Comments):
             let fixedWidth = tableView.frame.size.width - 2.0 * DiscussionCommentCell.marginX
             let label = UILabel()
@@ -228,24 +266,14 @@ class DiscussionCommentsViewController: UIViewController, UITableViewDataSource,
         }
     }
     
-    var commentInfoStyle : OEXTextStyle {
-        return OEXTextStyle(weight : .Normal, size : .Small, color : OEXStyles.sharedStyles().primaryBaseColor())
-    }
-    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(identifierCommentCell, forIndexPath: indexPath) as! DiscussionCommentCell
         
         // TODO factor these into the cell classes
         switch TableSection(rawValue: indexPath.section) {
         case .Some(.Response):
-            cell.bodyTextLabel.attributedText = largeTextStyle.attributedStringWithText(responseItem.body)
-            cell.authorLabel.attributedText = smallTextStyle.attributedStringWithText(responseItem.author)
-            cell.dateTimeLabel.attributedText = smallTextStyle.attributedStringWithText(responseItem.createdAt.timeAgoSinceNow())
+            cell.useResponse(responseItem)
             
-            let buttonTitle = NSAttributedString.joinInNaturalLayout([
-                Icon.Comment.attributedTextWithStyle(commentInfoStyle.withSize(.XSmall)),
-                commentInfoStyle.attributedStringWithText(NSString.oex_stringWithFormat(OEXLocalizedStringPlural("COMMENT", Float(comments.count), nil), parameters: ["count": Float(comments.count)]))])
-            cell.commentCountOrReportIconButton.setAttributedTitle(buttonTitle, forState: .Normal)
             return cell
         case .Some(.Comments):
             let comment = comments[indexPath.row]
@@ -254,11 +282,11 @@ class DiscussionCommentsViewController: UIViewController, UITableViewDataSource,
             if let createdAt = comment.createdAt {
                 cell.dateTimeLabel.attributedText = smallTextStyle.attributedStringWithText(createdAt.timeAgoSinceNow())
             }
-            cell.backgroundColor = OEXStyles.sharedStyles().neutralXLight()
+            cell.backgroundColor = OEXStyles.sharedStyles().neutralXXLight()
             
             let buttonTitle = NSAttributedString.joinInNaturalLayout([
-                Icon.ReportFlag.attributedTextWithStyle(commentInfoStyle.withSize(.XSmall)),
-                commentInfoStyle.attributedStringWithText(OEXLocalizedString("DISCUSSION_REPORT", nil))])
+                Icon.ReportFlag.attributedTextWithStyle(smallIconStyle),
+                smallTextStyle.attributedStringWithText(OEXLocalizedString("DISCUSSION_REPORT", nil))])
             cell.commentCountOrReportIconButton.setAttributedTitle(buttonTitle, forState: .Normal)
             cell.commentCountOrReportIconButton.row = indexPath.row
             cell.commentCountOrReportIconButton.oex_removeAllActions()
@@ -266,7 +294,7 @@ class DiscussionCommentsViewController: UIViewController, UITableViewDataSource,
                 if let owner = self, button = sender as? DiscussionCellButton, row = button.row {
                     let apiRequest = DiscussionAPI.flagComment(comment.flagged, commentID: comment.commentID)
                     
-                    owner.environment.router?.environment.networkManager.taskForRequest(apiRequest) { result in
+                    owner.environment.networkManager?.taskForRequest(apiRequest) { result in
                         if let comment: DiscussionComment = result.data {
                             // TODO: update UI
                         }
---
+++ Source/StandardViewControllerUIMethods.swift
@@ -0,0 +1,15 @@
+//
+//  StandardViewControllerUIMethods.swift
+//  edX
+//
+//  Created by Ehmad Zubair Chughtai on 02/09/2015.
+//  Copyright (c) 2015 edX. All rights reserved.
+//
+
+import Foundation
+
+protocol StandardViewControllerUIMethods {
+    func addSubviews()
+    func setStyles()
+    func setConstraints()
+}
