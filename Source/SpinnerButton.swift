db49fd3069b4c43a38def4d2348d3713722192c2
Ehmad Zubair Chughtai <ezubair@gmail.com>
2015-09-16 16:40:18 +0500

    Updated screenshots + added Spinner to DiscussionNewCommentViewController

--- Source/SpinnerButton.swift
+++ Source/SpinnerButton.swift
@@ -8,32 +8,42 @@
 
 import UIKit
 
+
+
 class SpinnerButton: UIButton {
-    let SPINNER_VIEW_TRAILING_MARGIN : CGFloat = 10
-    let VERTICAL_CONTENT_MARGINS : CGFloat = 20
+    private let SpinnerViewTrailingMargin : CGFloat = 10
+    private let VerticalContentMargin : CGFloat = 10
+    private let SpinnerHorizontalMargin : CGFloat = 10
+    private var SpinnerViewWidthWithMargins : CGFloat {
+        return spinnerView.intrinsicContentSize().width + 2 * SpinnerHorizontalMargin
+    }
     
-    let spinnerView = SpinnerView(size: .Large, color: .White)
+    private let spinnerView = SpinnerView(size: .Large, color: .White)
     
     override func layoutSubviews() {
         super.layoutSubviews()
         layoutSpinnerView()
     }
     
-    func layoutSpinnerView() {
+    private func layoutSpinnerView() {
         self.addSubview(spinnerView)
+        self.titleLabel?.adjustsFontSizeToFitWidth = true
+        
         spinnerView.snp_updateConstraints { (make) -> Void in
             make.centerY.equalTo(self)
             make.width.equalTo(spinnerView.intrinsicContentSize().width)
-            make.trailing.equalTo(self.snp_trailing).offset(-10).priorityHigh()
+            if let label = titleLabel {
+                make.leading.equalTo(label.snp_trailing).offset(SpinnerHorizontalMargin).priorityLow()
+            }
+            make.trailing.equalTo(self.snp_trailing).offset(-SpinnerHorizontalMargin).priorityHigh()
         }
         self.setNeedsUpdateConstraints()
         if !showProgress { spinnerView.hidden = true }
     }
     
     override func intrinsicContentSize() -> CGSize {
-        let width = self.titleLabel?.intrinsicContentSize().width ?? 0 + SPINNER_VIEW_TRAILING_MARGIN + self.spinnerView.intrinsicContentSize().width
-        let height = (self.titleLabel?.intrinsicContentSize().height ?? spinnerView.intrinsicContentSize().height) + VERTICAL_CONTENT_MARGINS
-        
+        let width = self.titleLabel?.intrinsicContentSize().width ?? 0 + SpinnerViewTrailingMargin + self.spinnerView.intrinsicContentSize().width
+        let height =  max(self.titleLabel?.intrinsicContentSize().height ?? 0, spinnerView.intrinsicContentSize().height) + 2 * VerticalContentMargin
         return CGSizeMake(width, height)
     }
     
@@ -47,8 +57,6 @@ class SpinnerButton: UIButton {
                 spinnerView.hidden = true
                 spinnerView.stopAnimating()
             }
-            self.setNeedsLayout()
         }
     }
-    
 }
