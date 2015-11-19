1ce3d1221d7287b1d0a691e9d4b19b1207311fb8
Michael Katz <katzman@gmail.com>
2015-11-19 11:36:49 -0500

    maybe add share button?

--- Source/CertificateViewController.swift
+++ Source/CertificateViewController.swift
@@ -7,7 +7,21 @@
 //
 
 import Foundation
+import WebKit
 
 class CertificateViewControlller: AuthenticatedWebViewController {
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let shareButton = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "share")
+        navigationItem.rightBarButtonItem = shareButton
+    }
+
+    func share() {
+        let text = "I took a course"
+        let url = (self.view as! WKWebView).URL!
+        let image = UIImage(named: "courseCertificate")!
+        let controller = UIActivityViewController(activityItems: [text,url,image], applicationActivities: nil)
+        presentViewController(controller, animated: true, completion: nil)
+    }
 }
