5eaef66c6f117de591e833e915a94480d7e14bc3
saeedbashir <saeed.aik@gmail.com>
2015-11-19 19:17:38 +0500

    download 'X' color changed to neutralBase
    https://openedx.atlassian.net/browse/MA-1676

--- Source/OEXDownloadTableCell.m
+++ Source/OEXDownloadTableCell.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014-2015 edX. All rights reserved.
 //
 
+#import "OEXStyles.h"
 #import "OEXDownloadTableCell.h"
 
 @implementation OEXDownloadTableCell
@@ -14,8 +15,14 @@
     [self.lbl_title setTextAlignment:NSTextAlignmentNatural];
     [self.lbl_time setTextAlignment:NSTextAlignmentNatural];
     [self.lbl_totalSize setTextAlignment:NSTextAlignmentNatural];
-    
     self.accessibilityTraits = UIAccessibilityTraitUpdatesFrequently;
+    [self tintCancelButton];
+}
+
+-(void) tintCancelButton {
+    UIImage *image = [_btn_cancel.imageView.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    [_btn_cancel setImage:image forState:UIControlStateNormal];
+    _btn_cancel.tintColor = [[OEXStyles sharedStyles] neutralBase];
 }
 
 @end
