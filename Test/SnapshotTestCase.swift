a7c45d9ca1c8ee0675e2141a2bf99538320003a3
Akiva Leffert <akiva@edx.org>
2016-02-16 17:09:16 -0500

    Loosen snapshot tolerance

--- Test/SnapshotTestCase.swift
+++ Test/SnapshotTestCase.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-private let StandardTolerance : CGFloat = 0.001
+private let StandardTolerance : CGFloat = 0.005
 
 protocol SnapshotTestable {
     func snapshotTestWithCase(testCase : FBSnapshotTestCase, referenceImagesDirectory: String, identifier: String) throws
