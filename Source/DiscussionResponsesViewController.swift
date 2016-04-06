0b3600e3163cc5ab237c54088982889cd59c80b2
Saeed Bashir <saeed.aik@gmail.com>
2016-04-06 16:27:45 +0500

    parenthesized author label

--- Source/DiscussionResponsesViewController.swift
+++ Source/DiscussionResponsesViewController.swift
@@ -741,7 +741,7 @@ extension AuthorLabelProtocol {
         attributedStrings.append(byAuthor)
         
         if let authorLabel = label {
-            attributedStrings.append(textStyle.attributedStringWithText(authorLabel))
+            attributedStrings.append(textStyle.attributedStringWithText("(\(authorLabel))"))
         }
         
         return NSAttributedString.joinInNaturalLayout(attributedStrings)
