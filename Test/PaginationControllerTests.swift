c5edfc96f06f22f07932fcdcc38f65f4fd17680c
Danial Zahid <danialzahid94@live.com>
2016-08-01 19:27:36 +0500

    Modify Pagination Loader (#765)
    * Modify footer visibility method
    
    * Modify Test Cases and Scrolling Conditions

--- Test/PaginationControllerTests.swift
+++ Test/PaginationControllerTests.swift
@@ -19,7 +19,7 @@ class PaginationControllerTests: XCTestCase {
         tableView.delegate = dataSource
         
         let paginator = WrappedPaginator<Int> { page in
-            let info = PaginationInfo(totalCount: 60, pageCount: 20)
+            let info = PaginationInfo(totalCount: 10, pageCount: 5)
             // Add a slight delay to make sure we get proper async behavior
             // to better match actual cases
             return Stream(value: Paginated(pagination: info, value: [1, 2, 3, 4, 5])).delay(0.1)
@@ -40,7 +40,7 @@ class PaginationControllerTests: XCTestCase {
         XCTAssertEqual(tableView.contentSize.height, CGFloat(initialCount) * dataSource.rowHeight)
         
         // Now scroll to the bottom
-        let bottomIndexPath = NSIndexPath(forRow: initialCount - 1, inSection: 0)
+        var bottomIndexPath = NSIndexPath(forRow: initialCount - 1, inSection: 0)
         tableView.scrollToRowAtIndexPath(bottomIndexPath, atScrollPosition: .Bottom, animated: false)
         XCTAssertNotNil(tableView.tableFooterView) // Should be showing spinner
 
@@ -48,6 +48,10 @@ class PaginationControllerTests: XCTestCase {
         waitForStream(paginationController.stream, fireIfAlreadyLoaded: false)
         let newCount = paginationController.stream.value?.count ?? 0
         XCTAssertGreaterThanOrEqual(newCount, initialCount)
+        
+        //Scrolling to the bottom second time to confirm that results are loaded
+        bottomIndexPath = NSIndexPath(forRow: newCount - 1, inSection: 0)
+        tableView.scrollToRowAtIndexPath(bottomIndexPath, atScrollPosition: .Bottom, animated: false)
         XCTAssertNil(tableView.tableFooterView) // Should not be showing spinner
         
     }
