8289a1692ced30f7a1543e56e062f8745531482a
Danial Zahid <danialzahid94@live.com>
2016-09-22 11:57:49 +0500

    Implement Feedback

--- Source/OEXRouter+Swift.swift
+++ Source/OEXRouter+Swift.swift
@@ -121,11 +121,6 @@ extension OEXRouter {
         }
     }
     
-    @available(*, deprecated=2.6, message="FullScreenMessageViewController has been deprecated in favor of UIAlertController to support accessibility") func showFullScreenMessageViewControllerFromViewController(controller : UIViewController, message : String, bottomButtonTitle: String?) {
-        let fullScreenViewController = FullScreenMessageViewController(message: message, bottomButtonTitle: bottomButtonTitle)
-        controller.presentViewController(fullScreenViewController, animated: true, completion: nil)
-    }
-    
     func showDiscussionResponsesFromViewController(controller: UIViewController, courseID : String, threadID : String) {
         let storyboard = UIStoryboard(name: "DiscussionResponses", bundle: nil)
         let responsesViewController = storyboard.instantiateInitialViewController() as! DiscussionResponsesViewController
