d2ee7f3235e9ef51a7c28a63b8a69937af9fe763
saeedbashir <saeed.aik@gmail.com>
2016-10-27 17:14:12 +0500

    All Videos showing empty list (#840)

--- Source/OEXMyVideosSubSectionViewController.m
+++ Source/OEXMyVideosSubSectionViewController.m
@@ -143,6 +143,8 @@ typedef NS_ENUM (NSUInteger, OEXAlertType) {
     [self setTitle:self.course.name];
     self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@" " style:UIBarButtonItemStylePlain target:self action:@selector(navigateBack)];
 
+    self.dataInterface = self.environment.interface;
+    
     //Init video view and video player
     self.videoPlayerInterface = [[OEXVideoPlayerInterface alloc] init];
     [self.videoPlayerInterface enableFullscreenAutorotation];
