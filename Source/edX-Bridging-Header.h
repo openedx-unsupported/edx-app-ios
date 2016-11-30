f95ed816cce4047948bd7181efa420fa9d430136
Saeed Bashir <saeed.aik@gmail.com>
2016-11-30 10:38:10 +0500

    import style

--- Source/edX-Bridging-Header.h
+++ Source/edX-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "SEGAnalytics.h"
+#import <Analytics/SEGAnalytics.h>
 #import "CLVideoPlayer.h"
 #import "CLButton.h"
 #import "DACircularProgressView.h"
