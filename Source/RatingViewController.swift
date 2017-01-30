74ed94e32332032d921bce895320044e9046cc0b
Danial Zahid <danialzahid94@live.com>
2017-01-30 15:21:54 +0500

    Add dismiss functionality

--- Source/RatingViewController.swift
+++ Source/RatingViewController.swift
@@ -53,9 +53,11 @@ class RatingViewController: UIViewController, RatingContainerDelegate {
     func didSelectRating(rating: CGFloat) {
         switch rating {
         case 1...3:
+            ratingContainerView.hidden = true
             negativeRatingReceived()
             break
         case 4...5:
+            ratingContainerView.hidden = true
             positiveRatingReceived()
             break
         default:
@@ -71,24 +73,33 @@ class RatingViewController: UIViewController, RatingContainerDelegate {
     func positiveRatingReceived() {
         let alertController = UIAlertController().showAlertWithTitle("Rate the app", message: "Tell others what you think of the edX app by writing a quick review in the app store",cancelButtonTitle: nil, onViewController: self)
         alertController.addButtonWithTitle("No Thanks") { (action) in
-            
+            self.dismissViewControllerAnimated(false, completion: nil)
         }
         alertController.addButtonWithTitle("Ask Me Later") { (action) in
-            
+            self.dismissViewControllerAnimated(false, completion: nil)
         }
         alertController.addButtonWithTitle("Rate The App") { (action) in
-            
+            self.sendUserToAppStore()
         }
+    }
+    
+    func sendUserToAppStore() {
+        guard let url = NSURL(string: "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=945480667&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software") else {
+            return
+        }
+        
+        UIApplication.sharedApplication().openURL(url)
     }
     
     //MARK: - Negative Rating methods
     func negativeRatingReceived() {
         let alertController = UIAlertController().showAlertWithTitle("Send Feedback?", message: "Help us improve!",cancelButtonTitle: nil, onViewController: self)
         alertController.addButtonWithTitle("Maybe Later") { (action) in
-            
+            self.dismissViewControllerAnimated(false, completion: nil)
         }
         alertController.addButtonWithTitle("Send Feedback") { (action) in
             
         }
+        environment.interface
     }
 }
