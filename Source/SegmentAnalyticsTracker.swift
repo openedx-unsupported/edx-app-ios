b9f59ea943692171c6a8e4bf355ea56dc1aee00b
Saeed Bashir <saeed.aik@gmail.com>
2017-01-30 12:58:53 +0500

    MA-3102: Firebase Analytics should not depend on Segment

--- Source/SegmentAnalyticsTracker.swift
+++ Source/SegmentAnalyticsTracker.swift
@@ -13,7 +13,6 @@ class SegmentAnalyticsTracker : NSObject, OEXAnalyticsTracker {
     private let GoogleCategoryKey = "category"
     private let GoogleLabelKey = "label"
     private let GoogleActionKey = "action"
-    private let firebaseTracker = FirebaseAnalyticsTracker.sharedTracker
     
     var currentOrientationValue : String {
         return UIInterfaceOrientationIsLandscape(UIApplication.sharedApplication().statusBarOrientation) ? OEXAnalyticsValueOrientationLandscape : OEXAnalyticsValueOrientationPortrait
@@ -60,10 +59,6 @@ class SegmentAnalyticsTracker : NSObject, OEXAnalyticsTracker {
         info[GoogleLabelKey] = event.label
         
         SEGAnalytics.sharedAnalytics().track(event.displayName, properties: info)
-        
-        if OEXConfig.sharedConfig().isFirebaseEnabled {
-            firebaseTracker.trackEventWithName(event.displayName, parameters: info as! [String : NSObject])
-        }
     }
     
     func trackScreenWithName(screenName: String, courseID: String?, value: String?, additionalInfo info: [String : String]?) {
