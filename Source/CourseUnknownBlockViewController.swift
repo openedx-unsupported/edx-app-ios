48d694158d570390d886efdd7fbf082a11a2e08b
Jeet <jeet.chanchawat@gmail.com>
2017-03-27 11:44:09 +0530

    Update CourseUnknownBlockViewController.swift
    Now the video will play when the response is a video url using the default AVKit and AVFoundation

--- Source/CourseUnknownBlockViewController.swift
+++ Source/CourseUnknownBlockViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class CourseUnknownBlockViewController: UIViewController, CourseBlockViewController {
     
@@ -18,6 +20,12 @@ class CourseUnknownBlockViewController: UIViewController, CourseBlockViewControl
     let courseID : String
     var block: CourseBlock?
     var messageView : IconMessageView?
+    
+    var player : AVPlayer? = nil
+    var playerLayer : AVPlayerLayer? = nil
+    var asset : AVAsset? = nil
+    var playerItem: AVPlayerItem? = nil
+    let playerViewController = AVPlayerViewController()
     
     var loader : Stream<NSURL?>?
     init(blockID : CourseBlockID?, courseID : String, environment : Environment) {
@@ -34,9 +42,23 @@ class CourseUnknownBlockViewController: UIViewController, CourseBlockViewControl
                 self?.block = block
                 if let video = block.type.asVideo where video.isYoutubeVideo{
                     self?.showYoutubeMessage(Strings.Video.viewOnYoutube, message: Strings.Video.onlyOnYoutube, icon: Icon.CourseModeVideo, videoUrl: video.videoURL)
-                }
-                else {
-                    self?.showError()
+                } else {
+                    // self?.showError()
+                    let video = block.type.asVideo
+                    let url:NSURL = NSURL(string: (video?.videoURL)!)!
+                    self!.asset = AVAsset(URL: url)
+                    self!.playerItem = AVPlayerItem(asset: self!.asset!)
+                    self!.player = AVPlayer(playerItem: self!.playerItem)
+                    self!.playerLayer = AVPlayerLayer(player: self!.player)
+                    self!.playerLayer!.frame = (self?.view.frame)!
+                    
+                    var frameRect = (self?.view.frame)!
+                    frameRect = CGRectMake(0 , 0, frameRect.width, frameRect.height-45)
+                    self!.playerViewController.view.frame =  frameRect
+                    self!.playerViewController.player = self!.player
+                    self!.view.addSubview(self!.playerViewController.view)
+
+                    self!.playVideoIfAvailable()
                 }
             },
             failure: {[weak self] _ in
@@ -83,6 +105,24 @@ class CourseUnknownBlockViewController: UIViewController, CourseBlockViewControl
         
         self.view.backgroundColor = OEXStyles.sharedStyles().standardBackgroundColor()
         
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideoIfAvailable()
+    }
+    
+    func playVideoIfAvailable() {
+        if self.playerViewController.player != nil {
+            self.playerViewController.player!.play()
+        }
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        if self.playerViewController.player != nil {
+            self.playerViewController.player!.pause()
+        }
     }
     
     override func updateViewConstraints() {
