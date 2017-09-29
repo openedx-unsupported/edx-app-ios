74cbd778cb5f4293d8eee5386bc6f81f6f5f8f27
salmannawaz <salman.nawaz@arbisoft.com>
2017-09-29 17:36:34 +0500

    LEARNER-2491, code improvment

--- Source/WhatsNewViewController.swift
+++ Source/WhatsNewViewController.swift
@@ -213,13 +213,8 @@ class WhatsNewViewController: UIViewController, UIPageViewControllerDelegate, UI
             if let item = dataModel.nextItem(currentItem: controller.whatsNew) {
                 return contentController(withItem: item, direction: .forward)
             }
-        
-            let totalScreens = dataModel.fields?.count ?? 0
-            if (dataModel.itemIndex(item: controller.whatsNew) == totalScreens-1) {
-                doneButton.isHidden = false
-            }
         }
-        
+        doneButton.isHidden = false
         return nil
     }
     
