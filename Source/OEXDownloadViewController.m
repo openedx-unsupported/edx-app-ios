344b89f823252bdee8e7e486620bbc538cffce73
salmannawaz <salman.nawaz@arbisoft.com>
2017-10-20 16:10:09 +0500

    Downloads - Showing unexpected information if there is no size and duration of any video. (#1022)
    * LEARNER-2737, Downloads - Showing unexpected information if there is no size and duration of any video, Fixed

--- Source/OEXDownloadViewController.m
+++ Source/OEXDownloadViewController.m
@@ -158,30 +158,28 @@
 - (void)configureCell:(OEXDownloadTableCell*)cell forIndexPath:(NSIndexPath*)indexPath {
     if([self.arr_downloadingVideo count] > indexPath.row) {
         OEXHelperVideoDownload* downloadingVideo = [self.arr_downloadingVideo objectAtIndex:indexPath.row];
-
-        
         NSString* videoName = downloadingVideo.summary.name;
         if([videoName length] == 0) {
             videoName = [Strings parenthesisWithText:[Strings untitled]];
         }
         cell.lbl_title.text = videoName;
 
-        if(!downloadingVideo.summary.duration) {
-            cell.lbl_time.text = [Strings myVideosTimeLabel];
+        if (downloadingVideo.summary.hasVideoDuration && downloadingVideo.summary.hasVideoSize) {
+            cell.lbl_time.text = [DateFormatting formatSecondsAsVideoLength: downloadingVideo.summary.duration];
+            cell.lbl_totalSize.text = [downloadingVideo.summary videoSize];
         }
-        else {
+        else if (downloadingVideo.summary.hasVideoDuration) {
             cell.lbl_time.text = [DateFormatting formatSecondsAsVideoLength: downloadingVideo.summary.duration];
         }
+        else if (downloadingVideo.summary.hasVideoSize) {
+            cell.lbl_time.text = [downloadingVideo.summary videoSize];
+        }
         
-        cell.lbl_totalSize.text = [downloadingVideo.summary videoSize];
         float progress = (float)downloadingVideo.downloadProgress;
         [cell.progressView setProgress:progress];
-        //
         cell.btn_cancel.tag = indexPath.row;
         cell.btn_cancel.accessibilityLabel = [Strings cancel];
-
         [cell.btn_cancel addTarget:self action:@selector(btnCancelPressed:) forControlEvents:UIControlEventTouchUpInside];
-        
         cell.accessibilityLabel = [self downloadStatusAccessibilityLabelForVideoName:videoName percentComplete:(progress / OEXMaxDownloadProgress)];
     }
 }
