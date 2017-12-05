6d6b36a5faf44b1e57c39d9549fda4f9e0b97d21
salmannawaz <salman.nawaz@arbisoft.com>
2017-12-05 22:28:12 +0500

    LEARNER-3061, Remove unnecessary call of showOfflineSnackBarIfNecessary.

--- Source/EnrolledCoursesViewController.swift
+++ Source/EnrolledCoursesViewController.swift
@@ -116,9 +116,6 @@ class EnrolledCoursesViewController : OfflineSupportViewController, CoursesTable
                     if enrollments.count <= 0 {
                         self?.enrollmentsEmptyState()
                     }
-                    else {
-                        self?.showOfflineSnackBarIfNecessary()
-                    }
                 }
                 else {
                     self?.loadController.state = .Initial
