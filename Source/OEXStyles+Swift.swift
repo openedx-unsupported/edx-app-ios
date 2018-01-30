a3d155c3c8e91a376d8cf66822b120328788e2c2
Saeed Bashir <saeed.aik@gmail.com>
2018-01-30 17:17:42 +0500

    Download Spiral TintColor with new app color scheme

--- Source/OEXStyles+Swift.swift
+++ Source/OEXStyles+Swift.swift
@@ -72,11 +72,9 @@ extension OEXStyles {
         return UIColor(red: CGFloat(126.0/255.0), green: CGFloat(199.0/255.0), blue: CGFloat(143.0/255.0), alpha: CGFloat(1.00))
     }
     
-    ///**Warning:** Not from style guide. Do not add more uses
     public var progressBarTrackTintColor : UIColor {
-        return UIColor(red: CGFloat(223.0/255.0), green: CGFloat(242.0/255.0), blue: CGFloat(228.0/255.0), alpha: CGFloat(1.00))
+        return OEXStyles.shared().neutralLight()
     }
-
 
     var standardTextViewInsets : UIEdgeInsets {
         return UIEdgeInsetsMake(8, 8, 8, 8)
