openedx-unsupported/edx-app-ios

commit 43fc32f17233feba2eb8859d3897569cbe9bc71f

Course Level Bulk Download Videos Option (#1064)

Source/CourseOutlineTableSource.swift
@@ -20,9 +20,9 @@ protocol CourseOutlineTableControllerDelegate : class {
     func outlineTableControllerReload(controller: CourseOutlineTableController)
 }
 
-class CourseOutlineTableController : UITableViewController, CourseVideoTableViewCellDelegate, CourseSectionTableViewCellDelegate {
-    
-    typealias Environment = DataManagerProvider & OEXInterfaceProvider & NetworkManagerProvider & OEXConfigProvider & OEXRouterProvider
+class CourseOutlineTableController : UITableViewController, CourseVideoTableViewCellDelegate, CourseSectionTableViewCellDelegate, CourseVideosHeaderViewDelegate {
+
+    typealias Environment = DataManagerProvider & OEXInterfaceProvider & NetworkManagerProvider & OEXConfigProvider & OEXRouterProvider & OEXAnalyticsProvider & OEXStylesProvider
     
     weak var delegate : CourseOutlineTableControllerDelegate?
     private let environment : Environment
@@ -30,12 +30,13 @@ class CourseOutlineTableController : UITableViewController, CourseVideoTableView
     let courseID : String
     private var courseOutlineMode: CourseOutlineMode
     
-    private let courseCard = CourseCardView(frame: CGRect.zero)
+    private let courseCard = CourseCardView(frame: .zero)
     private var courseCertificateView : CourseCertificateView?
     private let headerContainer = UIView()
-    private let lastAccessedView = CourseOutlineHeaderView(frame: CGRect.zero, styles: OEXStyles.shared(), titleText : Strings.lastAccessed, subtitleText : "Placeholder")
+    private let lastAccessedView = CourseOutlineHeaderView(frame: .zero, styles: OEXStyles.shared(), titleText : Strings.lastAccessed, subtitleText : "Placeholder")
+    var courseVideosHeaderView: CourseVideosHeaderView?
     private var lastAccess:Bool = false
-    private var shouldHideCourseCard:Bool = false
+    private var shouldHideTableViewHeader:Bool = false
     let refreshController = PullRefreshController()
     
     init(environment : Environment, courseID : String, forMode mode: CourseOutlineMode) {
@@ -78,18 +79,41 @@ class CourseOutlineTableController : UITableViewController, CourseVideoTableView
         tableView.register(CourseUnknownTableViewCell.self, forCellReuseIdentifier: CourseUnknownTableViewCell.identifier)
         tableView.register(CourseSectionTableViewCell.self, forCellReuseIdentifier: CourseSectionTableViewCell.identifier)
         tableView.register(DiscussionTableViewCell.self, forCellReuseIdentifier: DiscussionTableViewCell.identifier)
-        headerContainer.addSubview(lastAccessedView)
-        headerContainer.addSubview(courseCard)
-        addCertificateView()
-        
-        if let course = environment.interface?.enrollmentForCourse(withID: courseID)?.course, environment.config.isTabLayoutEnabled {
-            CourseCardViewModel.onCourseOutline(course: course).apply(card: courseCard, networkManager: environment.networkManager)
-            refreshTableHeaderView(lastAccess: false)
-            tableView.setAndLayoutTableHeaderView(header: headerContainer)
-            
-        }
-        
+        configureHeaderView()
         refreshController.setupInScrollView(scrollView: tableView)
+    }
+    
+    private func configureHeaderView() {
+        if let course = environment.interface?.enrollmentForCourse(withID: courseID)?.course {
+            switch courseOutlineMode {
+            case .full:
+                headerContainer.addSubview(lastAccessedView)
+                headerContainer.addSubview(courseCard)
+                addCertificateView()
+                CourseCardViewModel.onCourseOutline(course: course).apply(card: courseCard, networkManager: environment.networkManager)
+                if environment.config.isTabLayoutEnabled {
+                    refreshTableHeaderView(lastAccess: false)
+                }
+                break
+            case .video:
+                courseVideosHeaderView = CourseVideosHeaderView(with: course, environment: environment)
+                courseVideosHeaderView?.delegate = self
+                if let headerView = courseVideosHeaderView {
+                    headerContainer.addSubview(headerView)
+                }
+                
+                refreshTableHeaderView(lastAccess: false)
+                break
+            }
+        }
+    }
+    
+    func courseVideosHeaderViewTapped() {
+        delegate?.outlineTableControllerChoseShowDownloads(controller: self)
+    }
+    
+    func invalidDownloadSettings() {
+        showOverlay(withMessage: Strings.noWifiMessage)
     }
     
     private func indexPathForBlockWithID(blockID : CourseBlockID) -> NSIndexPath? {
@@ -112,6 +136,10 @@ class CourseOutlineTableController : UITableViewController, CourseVideoTableView
         if let highlightID = highlightedBlockID, let indexPath = indexPathForBlockWithID(blockID: highlightID)
         {
             tableView.scrollToRow(at: indexPath as IndexPath, at: UITableViewScrollPosition.middle, animated: false)
+        }
+        
+        if courseOutlineMode == .video {
+            courseVideosHeaderView?.refreshView()
         }
     }
     
@@ -161,7 +189,7 @@ class CourseOutlineTableController : UITableViewController, CourseVideoTableView
             cell.courseID = courseID
             cell.localState = environment.dataManager.interface?.stateForVideo(withID: block.blockID, courseID : courseQuerier.courseID)
             cell.delegate = self
-            cell.swipeCellViewDelegate = (courseOutlineMode == .Video) ? cell : nil
+            cell.swipeCellViewDelegate = (courseOutlineMode == .video) ? cell : nil
             return cell
         case .HTML(.Base):
             let cell = tableView.dequeueReusableCell(withIdentifier: CourseHTMLTableViewCell.identifier, for: indexPath) as! CourseHTMLTableViewCell
@@ -180,7 +208,7 @@ class CourseOutlineTableController : UITableViewController, CourseVideoTableView
             cell.block = nodes[indexPath.row]
             let courseID = courseQuerier.courseID
             cell.videos = courseQuerier.supportedBlockVideos(forCourseID: courseID, blockID: block.blockID)
-            cell.swipeCellViewDelegate = (courseOutlineMode == .Video) ? cell : nil
+            cell.swipeCellViewDelegate = (courseOutlineMode == .video) ? cell : nil
             cell.delegate = self
             cell.courseID = courseID
             return cell
@@ -262,50 +290,69 @@ class CourseOutlineTableController : UITableViewController, CourseVideoTableView
     }
     
     func hideTableHeaderView() {
-        shouldHideCourseCard = true
+        shouldHideTableViewHeader = true
         tableView.tableHeaderView = nil
     }
     
     private func refreshTableHeaderView(lastAccess: Bool) {
-        var constraintView: UIView = courseCard
-        if shouldHideCourseCard { return }
+        if shouldHideTableViewHeader { return }
         self.lastAccess = lastAccess
         lastAccessedView.isHidden = !lastAccess
         
-        courseCard.snp_remakeConstraints { (make) in
-            let screenWidth = UIScreen.main.bounds.size.width
-            if courseOutlineMode != .Full || !environment.config.isTabLayoutEnabled || shouldHideCourseCard {
-                make.height.equalTo(0)
+        switch courseOutlineMode {
+        case .full:
+            var constraintView: UIView = courseCard            
+            courseCard.snp_remakeConstraints { (make) in
+                let screenWidth = UIScreen.main.bounds.size.width
+                if courseOutlineMode != .full || !environment.config.isTabLayoutEnabled || shouldHideTableViewHeader {
+                    make.height.equalTo(0)
+                }
+                else {
+                    let screenHeight = UIScreen.main.bounds.size.height
+                    let halfScreenHeight = screenHeight / 2
+                    let ratioedHeight = screenWidth * defaultAspectRatio
+                    let _ = (halfScreenHeight > ratioedHeight) ? make.height.equalTo(ratioedHeight): make.height.equalTo(halfScreenHeight)
+                }
+                make.trailing.equalTo(headerContainer)
+                make.leading.equalTo(headerContainer)
+                make.width.equalTo(screenWidth)
+                make.top.equalTo(headerContainer)
             }
-            else {
-                let screenHeight = UIScreen.main.bounds.size.height
-                let halfScreehHeight = screenHeight / 2
-                let ratioedHeight = screenWidth * defaultAspectRatio
-                let _ = (halfScreehHeight > ratioedHeight) ? make.height.equalTo(ratioedHeight): make.height.equalTo(halfScreehHeight)
+            
+            if let courseCertificateView = courseCertificateView {
+                courseCertificateView.snp_remakeConstraints { (make) -> Void in
+                    make.trailing.equalTo(courseCard)
+                    make.leading.equalTo(courseCard)
+                    make.height.equalTo(CourseCertificateView.height)
+                    make.top.equalTo(constraintView.snp_bottom)
+                }
+                constraintView = courseCertificateView
             }
-            make.trailing.equalTo(headerContainer)
-            make.leading.equalTo(headerContainer)
-            make.width.equalTo(screenWidth)
-            make.top.equalTo(headerContainer)
-        }
-        if let courseCertificateView = courseCertificateView {
-            courseCertificateView.snp_remakeConstraints { (make) -> Void in
+            
+            lastAccessedView.snp_remakeConstraints { (make) -> Void in
                 make.trailing.equalTo(courseCard)
                 make.leading.equalTo(courseCard)
-                make.height.equalTo(CourseCertificateView.height)
                 make.top.equalTo(constraintView.snp_bottom)
+                let _ = lastAccess ? (isVerticallyCompact() ? make.height.equalTo(lassAccessViewLandscapeHeight) : make.height.equalTo(lassAccessViewPortraitHeight)) : make.height.equalTo(0)
+                make.bottom.equalTo(headerContainer)
             }
-            constraintView = courseCertificateView
+            tableView.setAndLayoutTableHeaderView(header: headerContainer)
+            break
+        case .video:
+            if let course = environment.interface?.enrollmentForCourse(withID: courseID)?.course,
+                environment.interface?.downloadableVideos(of: course).count ?? 0 <= 0 {
+                tableView.tableHeaderView = nil
+                return
+            }
+            courseVideosHeaderView?.snp_makeConstraints(closure: { make in
+                make.edges.equalTo(headerContainer)
+                make.height.equalTo(CourseVideosHeaderView.height)
+            })
+            courseVideosHeaderView?.refreshView()
+            tableView.setAndLayoutTableHeaderView(header: headerContainer)
+            break
         }
         
-        lastAccessedView.snp_remakeConstraints { (make) -> Void in
-            make.trailing.equalTo(courseCard)
-            make.leading.equalTo(courseCard)
-            make.top.equalTo(constraintView.snp_bottom)
-            let _ = lastAccess ? (isVerticallyCompact() ? make.height.equalTo(lassAccessViewLandscapeHeight) : make.height.equalTo(lassAccessViewPortraitHeight)) : make.height.equalTo(0)
-            make.bottom.equalTo(headerContainer)
-        }
-        tableView.setAndLayoutTableHeaderView(header: headerContainer)
     }
 }
 

Source/OEXInterface.m
@@ -42,9 +42,14 @@ NSString* const OEXCourseListKey = @"OEXCourseListKey";
 NSString* const OEXVideoStateChangedNotification = @"OEXVideoStateChangedNotification";
 NSString* const OEXDownloadProgressChangedNotification = @"OEXDownloadProgressChangedNotification";
 NSString* const OEXDownloadEndedNotification = @"OEXDownloadEndedNotification";
+NSString* const OEXDownloadStartedNotification = @"OEXDownloadStartedNotification";
+NSString* const OEXDownloadDeletedNotification = @"OEXDownloadDeletedNotification";
 NSString* const OEXSavedAppVersionKey = @"OEXSavedAppVersionKey";
 
-@interface OEXInterface () <OEXDownloadManagerProtocol>
+@interface OEXInterface () <OEXDownloadManagerProtocol> {
+    BOOL userAllowedLargeDownload;
+    DownloadVideosCompletionHandler downloadVideosCompletionHandler;
+}
 
 @property (nonatomic, strong) OEXNetworkInterface* network;
 @property (nonatomic, strong) OEXDataParser* parser;
@@ -334,34 +339,37 @@ static OEXInterface* _sharedInterface = nil;
     return YES;
 }
 
+- (BOOL) isDownloadSettingsValid {
+    OEXAppDelegate* appDelegate = (OEXAppDelegate *)[[UIApplication sharedApplication] delegate];
+    BOOL hasWifi = [appDelegate.reachability isReachableViaWiFi];
+    BOOL onlyOnWifi = [OEXInterface shouldDownloadOnlyOnWifi];
+    return !onlyOnWifi || hasWifi;
+}
+
 - (BOOL)canDownloadVideos:(NSArray*)videos {
     double totalSpaceRequired = 0;
     //Total space
     for(OEXHelperVideoDownload* video in videos) {
-        totalSpaceRequired += [video.summary.size doubleValue];
-    }
-    totalSpaceRequired = totalSpaceRequired / 1024 / 1024 / 1024;
-    OEXAppDelegate* appD = (OEXAppDelegate *)[[UIApplication sharedApplication] delegate];
-    if([OEXInterface shouldDownloadOnlyOnWifi]) {
-        if(![appD.reachability isReachableViaWiFi]) {
-            return NO;
+        if (video.downloadState == OEXDownloadStateNew) {
+            totalSpaceRequired += [video.summary.size doubleValue];
         }
     }
-    
-    if(totalSpaceRequired > 1) {
+    totalSpaceRequired = totalSpaceRequired / 1024 / 1024 / 1024;
+    if(!userAllowedLargeDownload && totalSpaceRequired > 1) {
         self.multipleDownloadArray = videos;
         
         // As suggested by Lou
         UIAlertView* alertView =
-            [[UIAlertView alloc] initWithTitle:[Strings largeDownloadTitle]
-                                       message:[Strings largeDownloadMessage]
-                                      delegate:self
-                             cancelButtonTitle:[Strings cancel]
-                             otherButtonTitles:[Strings acceptLargeVideoDownload], nil];
+        [[UIAlertView alloc] initWithTitle:[Strings largeDownloadTitle]
+                                   message:[Strings largeDownloadMessage]
+                                  delegate:self
+                         cancelButtonTitle:[Strings cancel]
+                         otherButtonTitles:[Strings acceptLargeVideoDownload], nil];
         
         [alertView show];
         return NO;
     }
+    userAllowedLargeDownload = false;
     return YES;
 }
 
@@ -373,14 +381,32 @@ static OEXInterface* _sharedInterface = nil;
     }
     
     NSInteger count = 0;
+    __block NSInteger downloadStartedCount = 0;
     for(OEXHelperVideoDownload* video in array) {
         if(video.summary.downloadURL.length > 0 && video.downloadState == OEXDownloadStateNew) {
             [self downloadAllTranscriptsForVideo:video];
-            [self addVideoForDownload:video completionHandler:^(BOOL success){}];
+            [self addVideoForDownload:video completionHandler:^(BOOL success){
+                downloadStartedCount++;
+                if (array.count == downloadStartedCount) {
+                    [[NSNotificationCenter defaultCenter] postNotificationName:OEXDownloadStartedNotification object:nil];
+                }
+            }];
             count++;
         }
+        else {
+            downloadStartedCount++;
+        }
+    }
+    if (downloadVideosCompletionHandler) {
+        downloadVideosCompletionHandler(false);
+        downloadVideosCompletionHandler = nil;
     }
     return count;
+}
+
+- (NSInteger)downloadVideos:(NSArray<OEXHelperVideoDownload*>*)array completionHandler: (DownloadVideosCompletionHandler) completionHandler  {
+    downloadVideosCompletionHandler = completionHandler;
+    return [self downloadVideos:array];
 }
 
 - (NSArray<OEXHelperVideoDownload*>*)statesForVideosWithIDs:(NSArray<NSString*>*)videoIDs courseID:(NSString*)courseID {
@@ -424,19 +450,23 @@ static OEXInterface* _sharedInterface = nil;
     [_storage markLastPlayedInterval:playedInterval forVideoID:videoId];
 }
 
-- (void)deleteDownloadedVideo:(OEXHelperVideoDownload *)video completionHandler:(void (^)(BOOL success))completionHandler {
+- (void)deleteDownloadedVideo:(OEXHelperVideoDownload *)video shouldNotify:(BOOL) shouldNotify completionHandler:(void (^)(BOOL success))completionHandler {
     [_storage deleteDataForVideoID:video.summary.videoID];
     video.downloadState = OEXDownloadStateNew;
     video.downloadProgress = 0.0;
     video.isVideoDownloading = false;
     completionHandler(YES);
+    if (shouldNotify) {
+        [[NSNotificationCenter defaultCenter] postNotificationName:OEXDownloadDeletedNotification object:nil];
+    }
 }
 
 - (void)deleteDownloadedVideos:(NSArray *)videos completionHandler:(void (^)(BOOL success))completionHandler {
     for (OEXHelperVideoDownload *video in videos) {
-        [self deleteDownloadedVideo:video completionHandler:^(BOOL success) {}];
+        [self deleteDownloadedVideo:video shouldNotify:false completionHandler:^(BOOL success) {}];
     }
     completionHandler(YES);
+    [[NSNotificationCenter defaultCenter] postNotificationName:OEXDownloadDeletedNotification object:nil];
 }
 
 - (void)setAllEntriesUnregister {
@@ -918,6 +948,7 @@ static OEXInterface* _sharedInterface = nil;
 
 - (void)alertView:(UIAlertView*)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
     if(buttonIndex == 1) {
+        userAllowedLargeDownload = true;
         NSInteger count = [self downloadVideos:_multipleDownloadArray];
         if(count > 0) {
             [[NSNotificationCenter defaultCenter] postNotificationName:FL_MESSAGE
@@ -927,6 +958,11 @@ static OEXInterface* _sharedInterface = nil;
     }
     else {
         self.multipleDownloadArray = nil;
+        userAllowedLargeDownload = false;
+        if (downloadVideosCompletionHandler) {
+            downloadVideosCompletionHandler(true);
+            downloadVideosCompletionHandler = nil;
+        }
     }
 }
 

commit 1115b8a22b566634cbae17ab9ace14a1d0611430

Enable Zombies and safety checks added for `getVideosForDownloadState` (#1073)
* Removing CLS_LOG logs and enabling zombies
* fixing broken tests cases that were introduced by https://github.com/edx/edx-app-ios/pull/1064

Source/CourseOutlineTableSource.swift
@@ -84,14 +84,18 @@ class CourseOutlineTableController : UITableViewController, CourseVideoTableView
     }
     
     private func configureHeaderView() {
+        
+        if courseOutlineMode == .full {
+            headerContainer.addSubview(lastAccessedView)
+            headerContainer.addSubview(courseCard)
+            addCertificateView()
+        }
+        
         if let course = environment.interface?.enrollmentForCourse(withID: courseID)?.course {
             switch courseOutlineMode {
             case .full:
-                headerContainer.addSubview(lastAccessedView)
-                headerContainer.addSubview(courseCard)
-                addCertificateView()
-                CourseCardViewModel.onCourseOutline(course: course).apply(card: courseCard, networkManager: environment.networkManager)
                 if environment.config.isTabLayoutEnabled {
+                    CourseCardViewModel.onCourseOutline(course: course).apply(card: courseCard, networkManager: environment.networkManager)
                     refreshTableHeaderView(lastAccess: false)
                 }
                 break

Source/OEXDBManager.m
@@ -196,12 +196,9 @@ static OEXDBManager* _sharedManager = nil;
 // Save all table data at a time
 - (void)saveCurrentStateToDB {
     OEXLogInfo(@"STORAGE", @"Save database context on main thread");
-    CLS_LOG(@"saveCurrentStateToDB");
     [[NSOperationQueue mainQueue] addOperationWithBlock:^{
         @synchronized(self.masterManagedObjectContext){
             [self.backGroundContext save:nil];
-            CLS_LOG(@"saveCurrentStateToDB: backGroundContext... %@", self.backGroundContext);
-            CLS_LOG(@"saveCurrentStateToDB: masterManagedObjectContext... %@", self.masterManagedObjectContext);
             [self.masterManagedObjectContext save:nil];
             NSError* error = nil;
             if(_masterManagedObjectContext != nil) {
@@ -225,18 +222,15 @@ static OEXDBManager* _sharedManager = nil;
 }
 
 - (NSArray*)executeFetchRequest:(NSFetchRequest*)fetchRequest {
-    CLS_LOG(@"executeFetchRequest");
     __block NSArray* resultArray;
     if([NSThread isMainThread]) {
         @synchronized(self.masterManagedObjectContext)
         {
-            CLS_LOG(@"executeFetchRequest: executeFetchRequest in main thread");
             resultArray = [self.masterManagedObjectContext executeFetchRequest:fetchRequest error:nil];
         }
     }
     else {
         @synchronized (self.backGroundContext) {
-            CLS_LOG(@"executeFetchRequest: executeFetchRequest in performBlockAndWait with %@", _backGroundContext);
             resultArray = [self.backGroundContext executeFetchRequest:fetchRequest error:nil];
         }
     }
@@ -404,11 +398,8 @@ static OEXDBManager* _sharedManager = nil;
 #pragma  mark - VideoData Table Methods
 
 - (NSArray*)getAllLocalVideoData {
-    CLS_LOG(@"getAllLocalVideoData");
     NSFetchRequest* fetchRequest = [[NSFetchRequest alloc] init];
-    CLS_LOG(@"getAllLocalVideoData: backGroundContext...%@", _backGroundContext);
     NSEntityDescription* videoEntity = [NSEntityDescription entityForName:@"VideoData" inManagedObjectContext:self.backGroundContext];
-    CLS_LOG(@"getAllLocalVideoData: getVideoEntity from manageObjectContect");
     [fetchRequest setEntity:videoEntity];
     return [self executeFetchRequest:fetchRequest];
 }
@@ -539,9 +530,7 @@ static OEXDBManager* _sharedManager = nil;
 
 - (void)pausedAllDownloads {
     NSArray* array = [self getAllLocalVideoData];
-    CLS_LOG(@"pausedAllDownloads: localVideoData...%@", array);
     for(VideoData* video in array) {
-        CLS_LOG(@"pausedAllDownloads: VideoData...%@", video);
         if((video && [video isKindOfClass:[VideoData class]]) && ([video.download_state intValue] == OEXDownloadStatePartial || [video.dm_id intValue] != 0)) {
             video.dm_id = [NSNumber numberWithInt:0];
         }
@@ -597,16 +586,21 @@ static OEXDBManager* _sharedManager = nil;
 }
 
 - (NSArray*)getVideosForDownloadState:(OEXDownloadState)state {
-    CLS_LOG(@"getVideosForDownloadState");
-    NSArray* allVideos = [self getAllLocalVideoData];
+    NSArray* allVideos = [[self getAllLocalVideoData] copy];
     NSMutableArray* filteredArray = [[NSMutableArray alloc] init];
-    CLS_LOG(@"getVideosForDownloadState: videosForDownloadState...%@", allVideos);
+    
     for(VideoData* data in allVideos) {
-        if(data && [data isKindOfClass:[VideoData class]] && ([data.download_state intValue] == state)) {
+        if(data &&
+           [data isEqual:[NSNull null]] &&
+           [data isKindOfClass:[VideoData class]] &&
+           data.download_state &&
+           [data.download_state isEqual:[NSNull null]] &&
+           [data.download_state isKindOfClass:[NSNumber class]] &&
+           ([data.download_state intValue] == state)) {
             [filteredArray addObject:data];
         }
     }
-
+    allVideos = nil;
     return filteredArray;
 }
 

Source/OEXDownloadManager.m
@@ -82,10 +82,8 @@ static NSURLSession* videosBackgroundSession = nil;
 - (void)resumePausedDownloads {
     __weak typeof(self) weakSelf = self;
     OEXLogInfo(@"DOWNLOADS", @"Resuming Paused downloads");
-    CLS_LOG(@"resumePausedDownloads");
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    dispatch_async(dispatch_get_main_queue(), ^{
         NSArray* array = [weakSelf.storage getVideosForDownloadState:OEXDownloadStatePartial];
-        CLS_LOG(@"resumePausedDownloads: videos get successfully");
         for(VideoData* data in array) {
             NSString* file = [OEXFileUtility filePathForVideoURL:data.video_url username:[OEXSession sharedSession].currentUser.username];
             if([[NSFileManager defaultManager] fileExistsAtPath:file]) {
@@ -94,9 +92,7 @@ static NSURLSession* videosBackgroundSession = nil;
             }
             [weakSelf downloadVideoForObject:data withCompletionHandler:^(NSURLSessionDownloadTask* downloadTask) {
                     if(downloadTask) {
-                        CLS_LOG(@"resumePausedDownloads: downloadTask");
                         data.dm_id = [NSNumber numberWithUnsignedInteger:downloadTask.taskIdentifier];
-                        CLS_LOG(@"resumePausedDownloads: get data.dm_id successfully");
                     }
                     else {
                         data.dm_id = [NSNumber numberWithInt:0];
@@ -104,32 +100,27 @@ static NSURLSession* videosBackgroundSession = nil;
                 }];
         }
         [weakSelf.storage saveCurrentStateToDB];
-        CLS_LOG(@"resumePausedDownloads: saveCurrentStateToDB successfully");
     });
 }
 
 //Start Download for video
 - (void)downloadVideoForObject:(VideoData*)video withCompletionHandler:(void (^)(NSURLSessionDownloadTask* downloadTask))completionHandler {
-    CLS_LOG(@"downloadVideoForObject");
     [self checkIfVideoIsDownloading:video withCompletionHandler:completionHandler];
 }
 
 // Start Download for video Url
 - (void)checkIfVideoIsDownloading:(VideoData*)video withCompletionHandler:(void (^)(NSURLSessionDownloadTask* downloadTask))completionHandler {
-    CLS_LOG(@"checkIfVideoIsDownloading");
     //Check if null
     if(!video.video_url || [video.video_url isEqualToString:@""]) {
         OEXLogError(@"DOWNLOADS", @"Download Manager Empty/Corrupt URL, ignoring");
         video.download_state = [NSNumber numberWithInt: OEXDownloadStateNew];
         video.dm_id = [NSNumber numberWithInt:0];
         [self.storage saveCurrentStateToDB];
-        CLS_LOG(@"checkIfVideoIsDownloading: saveCurrentStateToDB successfully");
         completionHandler(nil);
         return;
     }
 
     [videosBackgroundSession getTasksWithCompletionHandler:^(NSArray* dataTasks, NSArray* uploadTasks, NSArray* downloadTasks) {
-        CLS_LOG(@"checkIfVideoIsDownloading: getTasksWithCompletionHandler");
         //Check if already downloading
         BOOL alreadyInProgress = NO;
         __block NSInteger taskIndex = NSNotFound;
@@ -147,7 +138,6 @@ static NSURLSession* videosBackgroundSession = nil;
             video.download_state = [NSNumber numberWithInt:OEXDownloadStatePartial];
             video.dm_id = [NSNumber numberWithUnsignedInteger:downloadTask.taskIdentifier];
             [self.storage saveCurrentStateToDB];
-            CLS_LOG(@"checkIfVideoIsDownloading: saveCurrentStateToDB successfully");
             completionHandler(downloadTask);
         }
         else {
@@ -162,9 +152,7 @@ static NSURLSession* videosBackgroundSession = nil;
 }
 
 - (void)startDownloadForVideo:(VideoData*)video WithCompletionHandler:(void (^)(NSURLSessionDownloadTask* downloadTask))completionHandler {
-    CLS_LOG(@"startDownloadForVideo");
     NSURLSessionDownloadTask* _downloadTask = [self startBackgroundDownloadForVideo:video];
-    CLS_LOG(@"startDownloadForVideo: downloadTask");
     completionHandler(_downloadTask);
 }
 

Source/OEXInterface.m
@@ -53,7 +53,7 @@ NSString* const OEXSavedAppVersionKey = @"OEXSavedAppVersionKey";
 
 @property (nonatomic, strong) OEXNetworkInterface* network;
 @property (nonatomic, strong) OEXDataParser* parser;
-@property(nonatomic, weak) OEXDownloadManager* downloadManger;
+@property (nonatomic, strong) OEXDownloadManager* downloadManger;
 
 //Cached Data
 @property (nonatomic, assign) int commonDownloadProgress;

edX.xcodeproj/xcshareddata/xcschemes/edX.xcscheme
@@ -132,6 +132,13 @@
             ReferencedContainer = "container:edX.xcodeproj">
          </BuildableReference>
       </BuildableProductRunnable>
+      <EnvironmentVariables>
+         <EnvironmentVariable
+            key = "NSZombieEnabled"
+            value = "YES"
+            isEnabled = "YES">
+         </EnvironmentVariable>
+      </EnvironmentVariables>
       <AdditionalOptions>
       </AdditionalOptions>
    </LaunchAction>

Test/SessionUsernameProviderTests.swift
@@ -22,7 +22,7 @@ class SessionUsernameProviderTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
         let path = OEXFileUtility.t_path(forUserName: user.username!)
-        try! FileManager.default.removeItem(atPath: path)
+        try? FileManager.default.removeItem(atPath: path)
     }
 
     func providerForUsername(_ user : OEXUserDetails) -> SessionUsernameProvider {