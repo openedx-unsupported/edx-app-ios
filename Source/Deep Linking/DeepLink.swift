3ceef676882a18330650f2ea1bf0b4be2617f252
salmannawaz <salman.nawaz@arbisoft.com>
2018-10-16 16:11:41 +0500

    iOS : Implement deep linking for course dashboard (#1158)
    * LERNER-6444, Add deep link support for course dashboard screen.

---
+++ Source/Deep Linking/DeepLink.swift
@@ -0,0 +1,32 @@
+//
+//  DeepLink.swift
+//  edX
+//
+//  Created by Salman on 02/10/2018.
+//  Copyright © 2018 edX. All rights reserved.
+//
+
+import UIKit
+
+enum DeepLinkType: String {
+    case CourseDashboard = "course_dashboard"
+    case None = "none"
+}
+
+fileprivate enum DeepLinkKeys: String, RawStringExtractable {
+    case CourseId = "course_id"
+    case ScreenName = "screen_name"
+}
+
+class DeepLink: NSObject {
+
+    let courseId: String?
+    let screenName: String?
+    let type: DeepLinkType?
+    
+    init(dictionary:[String:Any]) {
+        courseId = dictionary[DeepLinkKeys.CourseId] as? String
+        screenName = dictionary[DeepLinkKeys.ScreenName] as? String
+        type = DeepLinkType(rawValue: screenName ?? DeepLinkType.None.rawValue) ?? .None
+    }
+}
---
+++ Source/Deep Linking/DeepLinkManager.swift
@@ -0,0 +1,57 @@
+//
+//  DeepLinkManager.swift
+//  edX
+//
+//  Created by Salman on 02/10/2018.
+//  Copyright © 2018 edX. All rights reserved.
+//
+
+import UIKit
+
+
+@objc class DeepLinkManager: NSObject {
+
+    static let sharedInstance = DeepLinkManager()
+    typealias Environment = OEXSessionProvider & OEXRouterProvider
+    var environment: Environment?
+    
+    private override init() {
+        super.init()
+    }
+
+    func processDeepLink(with params: [String: Any], environment: Environment) {
+        self.environment = environment
+        let deepLink = DeepLink(dictionary: params)
+        guard let deepLinkType = deepLink.type, deepLinkType != .None else {
+            return
+        }
+        
+        if isUserLoggedin() {
+            navigateToDeepLink(with: deepLinkType, link: deepLink)
+        }
+        else {
+            showLoginScreen()
+        }
+    }
+    
+    private func showLoginScreen() {
+        if let presentedViewController = UIApplication.shared.keyWindow?.rootViewController?.topMostController(), !(presentedViewController.childViewControllers[0].isKind(of: OEXLoginViewController.self)) {
+            presentedViewController.dismiss(animated: false, completion: nil)
+        }
+        environment?.router?.showLoginScreen(from: nil, completion: nil)
+    }
+    
+    private func isUserLoggedin() -> Bool {
+        return environment?.session.currentUser != nil
+    }
+    
+    private func navigateToDeepLink(with type: DeepLinkType, link: DeepLink) {
+            switch type {
+            case .CourseDashboard:
+                environment?.router?.showMyCourses(animated: true, pushingCourseWithID: link.courseId)
+                break
+            default:
+                break
+            }
+    }
+}
