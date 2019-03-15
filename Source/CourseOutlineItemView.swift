bb4e30d1cd90758ea88dbd6c8e796160fc5363e3
Saeed Bashir <saeed.aik@gmail.com>
2019-03-15 12:25:25 +0500

    Add Dynamic Type Support For Course Content Screen & Course List Screen

--- Source/CourseOutlineItemView.swift
+++ Source/CourseOutlineItemView.swift
@@ -116,6 +116,8 @@ public class CourseOutlineItemView: UIView {
             let formattedDateString = formattedDueDateString(asMonthDay: DateFormatting.date(withServerString: dueDate))
             attributedStrings.append(CourseOutlineItemView.detailFontStyle.attributedString(withText: formattedDateString))
         }
+        subtitleLabel.adjustsFontSizeToFitWidth = true
+        subtitleLabel.minimumScaleFactor = 0.6
         subtitleLabel.attributedText = NSAttributedString.joinInNaturalLayout(attributedStrings: attributedStrings)
         videoSizeLabel.attributedText = CourseOutlineItemView.detailFontStyle.attributedString(withText: videoSize)
         resetContraints(withBlockType: blockType)
@@ -171,6 +173,8 @@ public class CourseOutlineItemView: UIView {
             {
                 make.leading.equalTo(checkmark.snp.leading).offset(0)
             }
+
+            make.trailing.lessThanOrEqualTo(self).offset(-StandardHorizontalMargin)
         }
     }
     
