72e341f9310d33022cda56acfac986b6de559a1e
Saeed Bashir <saeed.aik@gmail.com>
2019-04-25 11:55:44 +0500

    Remove Firebase custom initialization. An option is available to Initialize the Firebase with `configureWithOptions` but if it is initialized with `configureWithOptions` then some of the Firebase KITS didn't work as expected. Especially Analytics didn't work well. More details about Analytics issue  can be found here https://github.com/firebase/quickstart-ios/issues/75

--- Test/FirebaseConfigTests.swift
+++ Test/FirebaseConfigTests.swift
@@ -17,10 +17,6 @@ class FirebaseConfigTests: XCTestCase {
         XCTAssertFalse(config.firebaseConfig.enabled)
         XCTAssertFalse(config.firebaseConfig.analyticsEnabled)
         XCTAssertFalse(config.firebaseConfig.cloudMessagingEnabled)
-        XCTAssertEqual(config.firebaseConfig.apiKey, "")
-        XCTAssertEqual(config.firebaseConfig.clientID, "")
-        XCTAssertEqual(config.firebaseConfig.gcmSenderID, "")
-        XCTAssertEqual(config.firebaseConfig.googleAppID, "")
         
     }
 
@@ -29,26 +25,14 @@ class FirebaseConfigTests: XCTestCase {
         XCTAssertFalse(config.firebaseConfig.enabled)
         XCTAssertFalse(config.firebaseConfig.analyticsEnabled)
         XCTAssertFalse(config.firebaseConfig.cloudMessagingEnabled)
-        XCTAssertEqual(config.firebaseConfig.apiKey, "")
-        XCTAssertEqual(config.firebaseConfig.clientID, "")
-        XCTAssertEqual(config.firebaseConfig.gcmSenderID, "")
-        XCTAssertEqual(config.firebaseConfig.googleAppID, "")
     }
 
     func testFirebaseConfig() {
-        let GCM_SENDER_ID = "608417025925"
-        let API_KEY = "Zhk6QiW7EbQW0WxJ5mzzZV9hDN8xEo"
-        let CLIENT_ID = "6i9smf15pi4baevepjrsscmbht9bg2ah.apps.googleusercontent.com"
-        let GOOGLE_APP_ID = "608417025925:ios:c04089bb49270266"
         let configDictionary = [
             "FIREBASE" : [
                 "ENABLED": true,
                 "ANALYTICS_ENABLED": true,
-                "CLOUD_MESSAGING_ENABLED": true,
-                "GCM_SENDER_ID": GCM_SENDER_ID,
-                "API_KEY": API_KEY,
-                "CLIENT_ID": CLIENT_ID,
-                "GOOGLE_APP_ID": GOOGLE_APP_ID
+                "CLOUD_MESSAGING_ENABLED": true
             ]
         ]
 
@@ -56,10 +40,6 @@ class FirebaseConfigTests: XCTestCase {
         XCTAssertTrue(config.firebaseConfig.enabled)
         XCTAssertTrue(config.firebaseConfig.analyticsEnabled)
         XCTAssertTrue(config.firebaseConfig.cloudMessagingEnabled)
-        XCTAssertEqual(config.firebaseConfig.apiKey, API_KEY)
-        XCTAssertEqual(config.firebaseConfig.clientID, CLIENT_ID)
-        XCTAssertEqual(config.firebaseConfig.gcmSenderID, GCM_SENDER_ID)
-        XCTAssertEqual(config.firebaseConfig.googleAppID, GOOGLE_APP_ID)
     }
 
     func testFirebaseDisableConfig() {
