16c0475b61f6d6a8f15c7c877455025d5ee4a5b8
salmannawaz <salman.nawaz@arbisoft.com>
2019-11-15 16:57:41 +0500

    LEARNER-7465, Fixed spaces

--- Source/FCMDataModel.swift
+++ Source/FCMDataModel.swift
@@ -14,18 +14,15 @@ fileprivate enum DataKeys: String, RawStringExtractable {
 }
 
 class FCMDataModel: NSObject {
-    
     let link: PushLink?
     
     init(dictionary:[String:Any]) {
         link = PushLink(dictionary: dictionary)
     }
-    
 }
 
 ///This link will have information of course and screen type which will be use by deeplink manager to route on particular screen.
 class PushLink: DeepLink {
-    
     let title: String?
     let body: String?
     
