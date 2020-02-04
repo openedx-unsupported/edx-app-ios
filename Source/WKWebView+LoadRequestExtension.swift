9a97c4d3075553be455d60e7fd212000d900a313
MuhammadUmer <mumer92@gmail.com>
2020-02-04 13:02:08 +0500

    fix github comments

---
+++ Source/WKWebView+LoadRequestExtension.swift
@@ -0,0 +1,81 @@
+//
+//  WKWebViewExtension.swift
+//  edX
+//
+//  Created by MuhammadUmer on 31/01/2020.
+//  Copyright © 2020 edX. All rights reserved.
+//
+
+import Foundation
+
+extension WKWebView {
+    private var languageCookieName: String {
+        return "prod-edx-language-preference"
+    }
+
+    private var defaultLanguage: String {
+        guard let language = NSLocale.preferredLanguages.first,
+            Bundle.main.preferredLocalizations.contains(language) else { return "en" }
+        return language
+    }
+    
+    func loadRequest(_ request: URLRequest) {
+        var request = request
+        
+        if #available(iOS 11.0, *) {
+            guard let domain = request.url?.rootDomain,
+                let languageCookie = HTTPCookie(properties: [
+                .domain: ".\(domain)",
+                .path: "/",
+                .name: languageCookieName,
+                .value: defaultLanguage,
+                .expires: NSDate(timeIntervalSinceNow: 3600000)
+                ])
+                else {
+                    load(request)
+                    return
+            }
+
+            getCookie(with: languageCookieName) { [weak self]  cookie in
+                if cookie == nil {
+                    self?.configuration.websiteDataStore.httpCookieStore.setCookie(languageCookie) {
+                        self?.load(request)
+                    }
+                }
+            }
+        } else {
+            request.addValue("\(languageCookieName)=\(defaultLanguage))", forHTTPHeaderField: "Cookie")
+        }
+        
+        load(request)
+    }
+}
+
+@available(iOS 11.0, *)
+extension WKWebView {
+    private var httpCookieStore: WKHTTPCookieStore  { return WKWebsiteDataStore.default().httpCookieStore }
+    
+    func getCookie(with name: String, completion: @escaping (HTTPCookie?)-> ()) {
+        httpCookieStore.getAllCookies { cookies in
+            for cookie in cookies {
+                if cookie.name.contains(name) {
+                    completion(cookie)
+                }
+            }
+        }
+        completion(nil)
+    }
+}
+
+extension URL {
+    var rootDomain: String? {
+        guard let hostName = host else { return nil }
+        
+        let components = hostName.components(separatedBy: ".")
+        if components.count > 2 {
+            return components.suffix(2).joined(separator: ".")
+        } else {
+            return hostName
+        }
+    }
+}
