ce5812d855c1b6745d0344fcebe4f355ea564f22
MuhammadUmer <mumer92@gmail.com>
2020-02-17 16:27:12 +0500

    microsoft social login

--- Source/MicrosoftSocial.swift
+++ Source/MicrosoftSocial.swift
@@ -7,20 +7,41 @@
 //
 
 import UIKit
-import MSAL
 
-typealias MSLoginCompletionHandler = (_ accessToken: String?, _ error: Error?) -> Void
+typealias MSLoginCompletionHandler =  (MSALAccount?, _ accessToken: String?, Error?) -> Void
+
+extension MSALError {
+    enum custom: Int {
+        case publicClientApplicationCreation = -500010
+        case noUserSignedIn = -500011
+        case userNotFound = -500012
+    }
+}
 
 class MicrosoftSocial: NSObject {
-
+    
     static let shared = MicrosoftSocial()
+    
+    private let kCurrentAccountIdentifier = "MSALCurrentAccountIdentifier"
+    private let kGraphURI = "https://graph.microsoft.com/v1.0/me/" // the Microsoft Graph endpoint
+    private var accessToken = String()
+    private var webViewParameters : MSALWebviewParameters?
     private var completionHandler: MSLoginCompletionHandler?
     private var applicationContext = MSALPublicClientApplication()
     var result: MSALResult?
     
     private let kAuthority = "https://login.microsoftonline.com/common/v2.0"
-    private let kScopes: [String] = ["https://graph.microsoft.com/user.read"]
-
+    private let kScopes = ["User.Read"]
+    
+    var currentAccountIdentifier: String? {
+        get {
+            return UserDefaults.standard.string(forKey: kCurrentAccountIdentifier)
+        }
+        set (accountIdentifier) {
+            UserDefaults.standard.set(accountIdentifier, forKey: kCurrentAccountIdentifier)
+        }
+    }
+    
     private override init() {
         super.init()
         NotificationCenter.default.oex_addObserver(observer: self, name: NSNotification.Name.OEXSessionEnded.rawValue) { (_, Observer, _) in
@@ -28,67 +49,79 @@ class MicrosoftSocial: NSObject {
         }
     }
     
-    func loginFromController(controller: UIViewController, completionHandler: @escaping MSLoginCompletionHandler) {
-        self.completionHandler = completionHandler
-        setUpApplicationContext()
+    func loginFromController(controller : UIViewController, completion: @escaping MSLoginCompletionHandler) {
         do {
-            // We check to see if we have a current signed-in user. If we don't, then we need to sign someone in.
-            // We throw an interactionRequired so that we trigger the interactive sign-in.
+            let clientApplication = try createClientApplication()
             
-            if try applicationContext.users().isEmpty {
-                throw NSError(domain: "MSALErrorDomain", code: MSALErrorCode.interactionRequired.rawValue, userInfo: nil)
-            } else {
-                // Acquire a token for an existing user silently.
-                // If the error comes and says interaction required then we call interative sign-in.
-                try applicationContext.acquireTokenSilent(forScopes: kScopes, user: applicationContext.users().first, authority: kAuthority) { [weak self] (result, error) in
-                    
-                    if let error  = error as NSError?, error.code == MSALErrorCode.interactionRequired.rawValue {
-                        self?.acquireTokenInteractively()
-                    }
-                    else {
-                        self?.handleResponse(result: result, error: error)
-                    }
+            let webParameters = MSALWebviewParameters(parentViewController: controller)
+            let parameters = MSALInteractiveTokenParameters(scopes: kScopes, webviewParameters: webParameters)
+            clientApplication.acquireToken(with: parameters) {
+                (result: MSALResult?, error: Error?) in
+                
+                guard let acquireTokenResult = result, error == nil else {
+                    completion(nil, nil, error)
+                    return
                 }
-
+                
+                // In the initial acquire token call we'll want to look at the account object
+                // that comes back in the result.
+                let signedInAccount = acquireTokenResult.account
+                self.currentAccountIdentifier = signedInAccount.homeAccountId?.identifier
+                
+                completion(signedInAccount, acquireTokenResult.accessToken, nil)
             }
-        } catch let error as NSError {
-            if error.code == MSALErrorCode.interactionRequired.rawValue {
-                acquireTokenInteractively()
-            }
-
-        } catch {
-            handleResponse(result: nil, error: error)
-        }
-    }
-
-    private func acquireTokenInteractively() {
-        applicationContext.acquireToken(forScopes: kScopes) { [weak self] (result, error) in
-            self?.handleResponse(result: result, error: error)
+        } catch let createApplicationError {
+            completion(nil, nil, createApplicationError)
         }
     }
     
-    private func handleResponse(result: MSALResult?, error: Error?) {
-        guard let result = result else {
-            completionHandler?(nil, error)
-            return
+    func createClientApplication() throws -> MSALPublicClientApplication {
+        // Initialize a MSALPublicClientApplication with a given clientID and authority
+        guard let kClientID = OEXConfig.shared().microsoftConfig.appID else {
+            throw NSError(domain: "MSALErrorDomain", code: MSALError.custom.publicClientApplicationCreation.rawValue, userInfo: nil)
+        }
+        // This MSALPublicClientApplication object is the representation of your app listing, in MSAL. For your own app
+        // go to the Microsoft App Portal to register your own applications with their own client IDs.
+        let config = MSALPublicClientApplicationConfig(clientId: kClientID)
+        do {
+            return try MSALPublicClientApplication(configuration: config)
+        } catch _ as NSError {
+            throw NSError(domain: "MSALErrorDomain", code: MSALError.custom.publicClientApplicationCreation.rawValue, userInfo: nil)
+        }
+    }
+    
+    @discardableResult func currentAccount() throws -> MSALAccount {
+        // We retrieve our current account by checking for the accountIdentifier that we stored in NSUserDefaults when
+        // we first signed in the account.
+        guard let accountIdentifier = currentAccountIdentifier else {
+            throw NSError(domain: "MSALErrorDomain", code: MSALError.custom.noUserSignedIn.rawValue, userInfo: nil)
         }
         
-        self.result = result
-        completionHandler?(result.accessToken, error)
-    }
-    
-    private func setUpApplicationContext() {
+        let clientApplication = try createClientApplication()
+        
+        var account: MSALAccount?
         do {
-            // Initialize a MSALPublicClientApplication with a given clientID and authority
-            let clientID = OEXConfig.shared().microsoftConfig.appID
-            applicationContext = try MSALPublicClientApplication(clientId: clientID, authority: kAuthority)
-        } catch let error as NSError {
-            completionHandler?(nil, error)
+            account = try clientApplication.account(forIdentifier: accountIdentifier)
+        } catch _ as NSError {
+            throw NSError(domain: "MSALErrorDomain", code: MSALError.custom.userNotFound.rawValue, userInfo: nil)
         }
+        
+        guard let currentAccount = account else {
+            clearCurrentAccount()
+            throw NSError(domain: "MSALErrorDomain", code: MSALError.custom.noUserSignedIn.rawValue, userInfo: nil)
+        }
+        
+        return currentAccount
     }
     
-    func requestUserProfileInfo(completion: (_ user: MSALUser) -> Void) {
-        guard let user = result?.user else {
+    func clearCurrentAccount() {
+        // Leave around the account identifier as the last piece of state to clean up as you will probably need
+        // it to clean up user-specific state
+        UserDefaults.standard.removeObject(forKey: kCurrentAccountIdentifier)
+    }
+    
+    func requestUserProfileInfo(completion: (_ user: MSALAccount) -> Void) {
+        guard let user = result?.account else {
             return
         }
         completion(user)
@@ -96,10 +129,22 @@ class MicrosoftSocial: NSObject {
     
     private func logout() {
         do {
-            // Removes all tokens from the cache for this application for the provided user
-            // first parameter:   The user to remove from the cache
-            try applicationContext.remove(self.applicationContext.users().first)
+            let accountToDelete = try? currentAccount()
             
+            // Signing out an account requires removing this from MSAL and cleaning up any extra state that the application
+            // might be maintaining outside of MSAL for the account.
+            
+            // This remove call only removes the account's tokens for this client ID in the local keychain cache. It does
+            // not sign the account completely out of the device or remove tokens for the account for other client IDs. If
+            // you have multiple applications sharing a client ID this will make the account effectively "disappear" for
+            // those applications as well if you are using Keychain Cache Sharing (not currently available in MSAL
+            // build preview). We do not recommend sharing a ClientID among multiple apps.
+            
+            if let accountToDelete = accountToDelete {
+                let application = try createClientApplication()
+                try application.remove(accountToDelete)
+            }
+            clearCurrentAccount()
         } catch let error {
             Logger.logError("Logout","Received error signing user out:: \(error)")
         }
