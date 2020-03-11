3222c793491dd7d941501e481b1f888c375e7ad5
Saeed Bashir <saeed.aik@gmail.com>
2020-03-11 12:24:34 +0500

    Remove reference of UIWebview and Branch SDK update

--- Pods/Branch/Branch-SDK/Branch-SDK/Networking/BNCServerInterface.m
+++ Pods/Branch/Branch-SDK/Branch-SDK/Networking/BNCServerInterface.m
@@ -481,9 +481,18 @@ exit:
     [self postRequest:post url:url retryNumber:0 key:key callback:callback];
 }
 
-- (BOOL) isV2APIURL:(NSString*)urlstring {
-    NSRange range = [urlstring rangeOfString:@"branch.io/v2/"];
-    return (range.location != NSNotFound);
+- (BOOL)isV2APIURL:(NSString *)urlstring {
+    return [self isV2APIURL:urlstring baseURL:[self.preferenceHelper branchAPIURL]];
+}
+
+- (BOOL)isV2APIURL:(NSString *)urlstring baseURL:(NSString *)baseURL {
+    BOOL found = NO;
+    if (urlstring && baseURL) {
+        NSString *matchString = [NSString stringWithFormat:@"%@/v2/", baseURL];
+        NSRange range = [urlstring rangeOfString:matchString];
+        found = (range.location != NSNotFound);
+    }
+    return found;
 }
 
 - (void)postRequest:(NSDictionary *)post
@@ -551,19 +560,25 @@ exit:
             // indicating various parts of the HTTP post failed.
             // We should retry in those conditions in addition to the case where the server returns a 500
 
-            BOOL isRetryableStatusCode = status >= 500 || status < 0;
+            // Status 53 means the request was killed by the OS because we're still in the background.
+            // This started happening in iOS 12 / Xcode 10 production when we're called from continueUserActivity:
+            // but we're not fully out of the background yet.
+
+            BOOL isRetryableStatusCode = status >= 500 || status < 0 || status == 53;
             
             // Retry the request if appropriate
             if (retryNumber < self.preferenceHelper.retryCount && isRetryableStatusCode) {
                 dispatch_time_t dispatchTime =
                     dispatch_time(DISPATCH_TIME_NOW, self.preferenceHelper.retryInterval * NSEC_PER_SEC);
                 dispatch_after(dispatchTime, dispatch_get_main_queue(), ^{
-                    BNCLogDebug(@"Retrying request with url %@", request.URL.relativePath);
-                    // Create the next request
-                    NSURLRequest *retryRequest = retryHandler(retryNumber);
-                    [self genericHTTPRequest:retryRequest
-                                 retryNumber:(retryNumber + 1)
-                                    callback:callback retryHandler:retryHandler];
+                    if (retryHandler) {
+                        BNCLogDebug(@"Retrying request with url %@", request.URL.relativePath);
+                        // Create the next request
+                        NSURLRequest *retryRequest = retryHandler(retryNumber);
+                        [self genericHTTPRequest:retryRequest
+                                     retryNumber:(retryNumber + 1)
+                                        callback:callback retryHandler:retryHandler];
+                    }
                 });
                 
                 // Do not continue on if retrying, else the callback will be called incorrectly
@@ -605,14 +620,9 @@ exit:
 
     if (Branch.trackingDisabled) {
         NSString *endpoint = request.URL.absoluteString;
-        BNCPreferenceHelper *prefs = [BNCPreferenceHelper preferenceHelper];
-        if (([endpoint bnc_containsString:@"/v1/install"] ||
-             [endpoint bnc_containsString:@"/v1/open"]) &&
-             ((prefs.linkClickIdentifier.length > 0 ) ||
-              (prefs.spotlightIdentifier.length > 0 ) ||
-              (prefs.universalLinkUrl.length > 0))) {
-            // Allow this network operation since it's an open/install to resolve a link.
-        } else {
+        
+        // if endpoint is not on the whitelist, fail it.
+        if (![self whiteListContainsEndpoint:endpoint]) {
             [[BNCPreferenceHelper preferenceHelper] clearTrackingInformation];
             NSError *error = [NSError branchErrorWithCode:BNCTrackingDisabledError];
             BNCLogError(@"Network service error: %@.", error);
@@ -622,6 +632,7 @@ exit:
             return;
         }
     }
+    
     id<BNCNetworkOperationProtocol> operation =
         [self.networkService networkOperationWithURLRequest:request.copy completion:completionHandler];
     [operation start];
@@ -633,6 +644,28 @@ exit:
         }
         return;
     }
+}
+
+- (BOOL)whiteListContainsEndpoint:(NSString *)endpoint {
+    BNCPreferenceHelper *prefs = [BNCPreferenceHelper preferenceHelper];
+    BOOL hasIdentifier = (prefs.linkClickIdentifier.length > 0 ) || (prefs.spotlightIdentifier.length > 0 ) || (prefs.universalLinkUrl.length > 0);
+    
+    // Allow install to resolve a link.
+    if ([endpoint bnc_containsString:@"/v1/install"] && hasIdentifier) {
+        return YES;
+    }
+    
+    // Allow open to resolve a link.
+    if ([endpoint bnc_containsString:@"/v1/open"] && hasIdentifier) {
+        return YES;
+    }
+    
+    // Allow short url creation requests
+    if ([endpoint bnc_containsString:@"/v1/url"]) {
+        return YES;
+    }
+    
+    return NO;
 }
 
 - (NSError*) verifyNetworkOperation:(id<BNCNetworkOperationProtocol>)operation {
