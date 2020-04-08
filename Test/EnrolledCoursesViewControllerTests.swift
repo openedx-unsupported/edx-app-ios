bf250a236aba2499babea821c3b874df8893324c
MuhammadUmer <mumer92@gmail.com>
2020-04-08 16:10:37 +0500

    add testCourseListCourseDiscoveryEnabled

--- Test/EnrolledCoursesViewControllerTests.swift
+++ Test/EnrolledCoursesViewControllerTests.swift
@@ -58,6 +58,27 @@ class EnrolledCoursesViewControllerTests: SnapshotTestCase {
         }
     }
     
+    func testCourseListCourseDiscoveryEnabled() {
+        let courses = [OEXCourse.freshCourse(), OEXCourse.freshCourse()]
+        let configDictionary = [
+            "DISCOVERY": [
+                "COURSE": [
+                    "TYPE": "webview",
+                ]
+            ]
+        ]
+        
+        let config =  OEXConfig(dictionary: configDictionary)
+        
+        let environment = TestRouterEnvironment(config: config, interface: nil).logInTestUser()
+        environment.mockEnrollmentManager.courses = courses
+        let controller = EnrolledCoursesViewController(environment: environment)
+        inScreenNavigationContext(controller) {
+            waitForStream(controller.t_loaded)
+            assertSnapshotValidWithContent(controller.navigationController!)
+        }
+    }
+    
     func testAnalyticsEmitted() {
         let environment = TestRouterEnvironment().logInTestUser()
         environment.mockEnrollmentManager.enrollments = []
