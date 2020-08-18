c6247a966f471b4cfd54542e7faa0478c9cf4015
Muhammad Umer <mumer92@gmail.com>
2020-08-18 17:43:10 +0500

    refactor test cases

--- Test/CourseDatesModelTests.swift
+++ Test/CourseDatesModelTests.swift
@@ -11,9 +11,18 @@ import XCTest
 @testable import edX
 
 class CourseDatesModelTests: XCTestCase {
+    let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates"))
+    
+    func testCourseDates() {
+        guard let _ = courseDates else {
+            XCTFail("Expected Course Dates Not Available")
+            return
+        }
+        XCTAssert(true, "Expected Course Dates Available")
+    }
+    
     func testDatesIsInToday() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else { return }
-        var blocks = courseDates.courseDateBlocks
+        var blocks = courseDates!.courseDateBlocks
         
         let isToday = blocks.first { $0.blockStatus == .today }
         
@@ -39,120 +48,74 @@ class CourseDatesModelTests: XCTestCase {
         }
     }
     
+    func testDatesIsInNotToday() {
+        let isToday = courseDates!.courseDateBlocks.first { $0.blockStatus == .today }
+        XCTAssert(isToday == nil, "Expected Course Date in Not Today")
+    }
+    
     func testDatesIsInPast() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates in past Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.isInPast }
+        let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates"))
+        let blocks = courseDates!.courseDateBlocks.filter { $0.isInPast }
         XCTAssert(blocks.count > 0, "Expected Course Dates in past")
     }
     
     func testDatesIsInFuture() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates in future Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.isInFuture }
+        let blocks = courseDates!.courseDateBlocks.filter { $0.isInFuture }
         XCTAssert(blocks.count > 0, "Expected Course Dates in future")
     }
     
     func testDatesIsPastDue() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates is Past Due Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.blockStatus == .pastDue }
+        let blocks = courseDates!.courseDateBlocks.filter { $0.blockStatus == .pastDue }
         XCTAssert(blocks.count > 0, "Expected Course Dates is Past Due")
     }
     
     func testDatesIsDueNext() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates is Due Next Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.blockStatus == .dueNext }
+        let blocks = courseDates!.courseDateBlocks.filter { $0.blockStatus == .dueNext }
         XCTAssert(blocks.count > 0, "Expected Course Dates is Due Next")
     }
     
     func testDatesLearnerHasAccess() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates Learner Has Access Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.learnerHasAccess }
+        let blocks = courseDates!.courseDateBlocks.filter { $0.learnerHasAccess }
         XCTAssert(blocks.count > 0, "Expected Course Dates Learner Has Access")
     }
     
     func testDatesShowLink() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates Show Link Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.showLink }
+        let blocks = courseDates!.courseDateBlocks.filter { $0.showLink }
         XCTAssert(blocks.count > 0, "Expected Course Dates Show Link")
     }
     
     func testDatesUnreleased() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates Unreleased Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.isUnreleased }
+        let blocks = courseDates!.courseDateBlocks.filter { $0.isUnreleased }
         XCTAssert(blocks.count > 0, "Expected Course Dates Unreleased")
     }
     
     func testDatesIsReleased() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates Released Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { !$0.isUnreleased }
+        let blocks = courseDates!.courseDateBlocks.filter { !$0.isUnreleased }
         XCTAssert(blocks.count > 0, "Expected Course Dates Released")
     }
     
     func testDatesIsAvailable() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates is Available Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.available }
+        let blocks = courseDates!.courseDateBlocks.filter { $0.available }
         XCTAssert(blocks.count > 0, "Expected Course Dates is Available")
     }
     
     func testDatesIsAssignment() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else  {
-            XCTFail("Expected Course Dates is Assignment Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.isAssignment }
+        let blocks = courseDates!.courseDateBlocks.filter { $0.isAssignment }
         XCTAssert(blocks.count > 0, "Expected Course Dates is Assignment")
     }
     
     func testDatesIsLearnerAssignment() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else  {
-            XCTFail("Expected Course Dates Learner has Access and is Assignment Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.isLearnerAssignment }
+        let blocks = courseDates!.courseDateBlocks.filter { $0.isLearnerAssignment }
         XCTAssert(blocks.count > 0, "Expected Course Dates Learner has Access and is Assignment")
     }
     
     func testDatesHasDescription() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates Has Description Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { $0.hasDescription }
+        let blocks = courseDates!.courseDateBlocks.filter { $0.hasDescription }
         XCTAssert(blocks.count > 0, "Expected Course Dates Has Description")
     }
     
     func testDatesIsVerifiedOnly() {
-        guard let courseDates = CourseDateModel(json: JSON(resourceNamed: "CourseDates")) else {
-            XCTFail("Expected Course Dates is Verified Only Failed")
-            return
-        }
-        let blocks = courseDates.courseDateBlocks.filter { !$0.isLearnerAssignment }
+        let blocks = courseDates!.courseDateBlocks.filter { !$0.isLearnerAssignment }
         XCTAssert(blocks.count > 0, "Expected Course Dates is Verified Only")
     }
 }
