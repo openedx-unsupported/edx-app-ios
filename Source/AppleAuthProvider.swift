cc2f2200090c6050a4424576834bfa40a940aba9
salmannawaz <salman.nawaz@arbisoft.com>
2020-08-28 11:32:00 +0500

    LEARNER-7871, Fixed comment

--- Source/AppleAuthProvider.swift
+++ Source/AppleAuthProvider.swift
@@ -25,7 +25,6 @@ class AppleAuthProvider: NSObject, OEXExternalAuthProvider {
     func freshAuthButton() -> UIButton {
         let button = OEXExternalAuthProviderButton()
         button.provider = self
-        button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
         button.setImage(UIImage(named: "icon_apple"), for: .normal)
         button.useBackgroundImage(of: UIColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1.0))
         return button
