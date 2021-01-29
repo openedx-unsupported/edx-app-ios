f9871a472bd2d2ccb4fbeab866f78eb8a0179bee
Muhammad Umer <mumer92@gmail.com>
2021-01-29 10:55:24 +0500

    refactor hiding

--- Source/CourseOutlineViewController.swift
+++ Source/CourseOutlineViewController.swift
@@ -203,7 +203,7 @@ public class CourseOutlineViewController :
                         }
                     }
                 } else {
-                    self?.tableController.hideCourseDateBanner()
+                    self?.hideCourseBannerView()
                 }
                 
                 break
