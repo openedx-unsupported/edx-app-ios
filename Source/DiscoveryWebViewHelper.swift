3348942cbdbad5ceda7f22a16a66374a84b5def5
Colin Brash <cbrash@edx.org>
2021-02-16 19:32:19 -0500

    See if the WebViewDelegate wants to handle a navigation action before trying other ways of handling.
    This fixes an issue introduced by webpages using Segment's `trackLink()`.  `trackLink()` programmatically changes `window.location.href`, which registers as a navigation action of type `.other`.

--- Source/DiscoveryWebViewHelper.swift
+++ Source/DiscoveryWebViewHelper.swift
@@ -278,16 +278,20 @@ class DiscoveryWebViewHelper: NSObject {
 extension DiscoveryWebViewHelper: WKNavigationDelegate {
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         let request = navigationAction.request
-        let capturedLink = navigationAction.navigationType == .linkActivated && (delegate?.webView(webView, shouldLoad: request) ?? true)
         
-        let outsideLink = (request.mainDocumentURL?.host != self.request?.url?.host)
-        if let URL = request.url, outsideLink || capturedLink {
-            UIApplication.shared.open(URL, options: [:], completionHandler: nil)
+        let isWebViewDelegateHandled = !(delegate?.webView(webView, shouldLoad: request) ?? true)
+        if isWebViewDelegateHandled {
             decisionHandler(.cancel)
-            return
+        } else {
+            let capturedLink = navigationAction.navigationType == .linkActivated
+            let outsideLink = (request.mainDocumentURL?.host != self.request?.url?.host)
+            if let URL = request.url, outsideLink || capturedLink {
+                UIApplication.shared.open(URL, options: [:], completionHandler: nil)
+                decisionHandler(.cancel)
+            } else {
+                decisionHandler(.allow)
+            }
         }
-        
-        decisionHandler(.allow)
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
