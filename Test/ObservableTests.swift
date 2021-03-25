176cd18c252d41cbe41083dbd0aa90cb3b01671a
Muhammad Umer <mumer92@gmail.com>
2021-03-25 16:20:18 +0500

    added observeable tests

--- Test/ObservableTests.swift
+++ Test/ObservableTests.swift
@@ -10,9 +10,31 @@ import Foundation
 import XCTest
 @testable import edX
 
+fileprivate class ObserveableTestModel {
+    var value: Int
+    var observer: Observable<Int>
+    
+    init(value: Int) {
+        self.value = value
+        self.observer = Observable(value)
+    }
+}
+
 class ObservableTests: XCTestCase {
-    func testObserable() {
-        //TODO: add tests
-        XCTAssertTrue(true)
+    
+    func testObserableNewValue() {
+        let model = ObserveableTestModel(value: 3)
+        model.observer.subscribe(observer: self) { newValue, oldValue in
+            XCTAssert(newValue == 5)
+        }
+        model.value = 5
+    }
+    
+    func testObserableOldValue() {
+        let model = ObserveableTestModel(value: 3)
+        model.observer.subscribe(observer: self) { newValue, oldValue in
+            XCTAssert(oldValue == 3)
+        }
+        model.value = 5
     }
 }
