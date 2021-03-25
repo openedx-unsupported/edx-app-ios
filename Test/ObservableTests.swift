e6c60a62f4a200d4f99c773ca24f7f1f3532bb54
Saeed Bashir <saeed.aik@gmail.com>
2021-03-25 15:56:49 +0500

    Add ObservableTests file

---
+++ Test/ObservableTests.swift
@@ -0,0 +1,18 @@
+//
+//  ObservableTests.swift
+//  edXTests
+//
+//  Created by Saeed Bashir on 3/25/21.
+//  Copyright © 2021 edX. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import edX
+
+class ObservableTests: XCTestCase {
+    func testObserable() {
+        //TODO: add tests
+        XCTAssertTrue(true)
+    }
+}
