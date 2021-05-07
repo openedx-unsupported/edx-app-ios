e1716175984a6024fe5e74d9e55c38c38b1cd470
Saeed Bashir <saeed.aik@gmail.com>
2021-05-07 09:15:22 +0500

    Spinner animation disappear issue fixed

--- Source/SpinnerView.swift
+++ Source/SpinnerView.swift
@@ -87,14 +87,20 @@ public class SpinnerView: UIView {
     }
     
     func startAnimating() {
-        activityIndicator?.removeFromSuperview()
-        activityIndicator = MaterialActivityIndicatorView()
-        if let view = activityIndicator {
-            addSubview(view)
+        DispatchQueue.main.async { [weak self] in
+            self?.animate()
         }
-        activityIndicator?.color = color.value
-        activityIndicator?.startAnimating()
     }
+
+    private func animate() {
+            activityIndicator?.removeFromSuperview()
+            activityIndicator = MaterialActivityIndicatorView()
+            if let view = activityIndicator {
+                addSubview(view)
+            }
+            activityIndicator?.color = color.value
+            activityIndicator?.startAnimating()
+        }
     
     func stopAnimating() {
         activityIndicator?.removeFromSuperview()
