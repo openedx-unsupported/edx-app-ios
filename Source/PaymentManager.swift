c2f702dcd0f26ba12507ceb423ecd1bd223395c8
Muhammad Umer <mumer92@gmail.com>
2021-06-22 13:03:34 +0500

    chore

---
+++ Source/PaymentManager.swift
@@ -0,0 +1,15 @@
+//
+//  PaymentManager.swift
+//  edX
+//
+//  Created by Muhammad Umer on 22/06/2021.
+//  Copyright © 2021 edX. All rights reserved.
+//
+
+import Foundation
+
+class PaymentManager {
+    static let shared = PaymentManager()
+    
+    private init() { }
+}
