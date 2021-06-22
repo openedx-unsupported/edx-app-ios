773a413b261b7ece9874fa3bc454703a8911f9bb
Muhammad Umer <mumer92@gmail.com>
2021-06-22 15:16:03 +0500

    chore: PaymentManager

--- Source/PaymentManager.swift
+++ Source/PaymentManager.swift
@@ -7,9 +7,43 @@
 //
 
 import Foundation
+import SwiftyStoreKit
 
 class PaymentManager {
     static let shared = PaymentManager()
     
-    private init() { }
+    private init() {
+        setupIAP()
+    }
+    
+    func setupIAP() {
+        SwiftyStoreKit.completeTransactions(atomically: true) { purchases in
+            for purchase in purchases {
+                switch purchase.transaction.transactionState {
+                case .purchased, .restored:
+                    let downloads = purchase.transaction.downloads
+                    if !downloads.isEmpty {
+                        SwiftyStoreKit.start(downloads)
+                    } else if purchase.needsFinishTransaction {
+                        // Deliver content from server, then:
+                        SwiftyStoreKit.finishTransaction(purchase.transaction)
+                    }
+                    print("\(purchase.transaction.transactionState.debugDescription): \(purchase.productId)")
+                case .failed, .purchasing, .deferred:
+                    break // do nothing
+                @unknown default:
+                    break // do nothing
+                }
+            }
+        }
+        
+        SwiftyStoreKit.updatedDownloadsHandler = { downloads in
+            // contentURL is not nil if downloadState == .finished
+            let contentURLs = downloads.compactMap { $0.contentURL }
+            if contentURLs.count == downloads.count {
+                print("Saving: \(contentURLs)")
+                SwiftyStoreKit.finishTransaction(downloads[0].transaction)
+            }
+        }
+    }
 }
