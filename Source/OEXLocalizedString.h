11daef2bb955883ed90e8c4d8feb6089d6bb6d52
Muhammad Umer <mumer92@gmail.com>
2021-11-23 14:46:18 +0500

    refactor: address feedback

--- Source/OEXLocalizedString.h
+++ Source/OEXLocalizedString.h
@@ -13,8 +13,8 @@ NS_ASSUME_NONNULL_BEGIN
 /// Use this for *all* strings displayed to the user
 /// unless they need to handle plurals in which case see OEXLocalizedStringPlural
 /// The string should be defined in Localizable.strings
+/// With Table name, which should be current file name, e.g. ProfileOptions
 /// Along with a comment there.
-NSString* OEXLocalizedString(NSString* string,  NSString* __nullable  comment);
 
 NSString* OEXLocalizedStringFromTable(NSString* string, NSString* tbl, NSString* __nullable  comment);
 
@@ -22,9 +22,9 @@ NSString* OEXLocalizedStringFromTable(NSString* string, NSString* tbl, NSString*
 /// For other strings see OEXLocalizedString
 /// The string should be defined in Localizable.strings
 /// Along with a comment there.
+/// With Table name, which should be current file name, e.g. ProfileOptions
 /// For full documentation on the format to deal with the different plural cases see
 /// https://github.com/Smartling/ios-i18n
-NSString* OEXLocalizedStringPlural(NSString* key, NSInteger value,  NSString* __nullable  comment);
 
 NSString* OEXLocalizedStringPluralFromTable(NSString* key, NSString* tbl, NSInteger value,  NSString* __nullable  comment);
 
--- Source/OEXLocalizedString.m
+++ Source/OEXLocalizedString.m
@@ -10,22 +10,10 @@
 
 #import <Smartling.i18n/SLLocalization.h>
 
-NSString* OEXLocalizedString(NSString* key, NSString* comment) {
-    NSString* result = NSLocalizedString(key, comment);
-    return result;
-}
-
 NSString* OEXLocalizedStringFromTable(NSString* key, NSString* tbl, NSString* comment) {
-    NSString* result = NSLocalizedStringFromTable(key, tbl, comment);
-    return result;
-}
-
-NSString* OEXLocalizedStringPlural(NSString* key, NSInteger value, NSString* comment) {
-    NSString* result = SLPluralizedString(key, value, comment);
-    return result;
+    return NSLocalizedStringFromTable(key, tbl, comment);
 }
 
 NSString* OEXLocalizedStringPluralFromTable(NSString* key, NSString* tbl, NSInteger value, NSString* comment) {
-    NSString* result = SLPluralizedStringFromTable(key, tbl, value, comment);
-    return result;
+    return SLPluralizedStringFromTable(key, tbl, value, comment);
 }
