86e02f8225afae8f8a9eba36adc2ac0e5ea5b04e
Muhammad Umer <mumer92@gmail.com>
2021-11-23 17:13:26 +0500

    Revert "build: separate out localization file depending on feature  (#1570)" (#1573)
    This reverts commit b4ca5007cde1c548a2fe82f882a552c4c9ab0dd7.

--- script/Localizations.swift
+++ script/Localizations.swift
@@ -1,17 +1,65 @@
 #!/usr/bin/xcrun -sdk macosx swift
+
 import Foundation
 
-var topLevel = Group(name:"Strings")
-var topLevelItems: [Key : Item] = [:]
+//MARK: Helpers
 
 // Stream that writes to stderr.
 class StandardErrorOutputStream: TextOutputStream {
     func write(_ string: String) {
         fputs(string, stderr)
     }
+    
 }
 
 var errorStream = StandardErrorOutputStream()
+
+// This makes it easy to ``print`` to a file
+class FileOutputStream : TextOutputStream {
+    let handle : FileHandle
+    init?(path : String) {
+        if !FileManager.default.createFile(atPath: path, contents:nil, attributes:nil) {
+            self.handle = FileHandle.nullDevice
+            return nil
+        }
+        if let handle = FileHandle(forWritingAtPath:path) {
+            self.handle = handle
+        }
+        else {
+            self.handle = FileHandle.nullDevice
+            return nil
+        }
+    }
+    
+    func write(_ string: String) {
+        handle.write(string.data(using: String.Encoding.utf8)!)
+    }
+    
+    deinit {
+        handle.closeFile()
+    }
+}
+
+// MARK: Load
+
+let arguments = CommandLine.arguments
+guard arguments.count > 2 else {
+    print("error: Not enough arguments. Aborting")
+    print("Usage: ")
+    print("Localizations.swift <source_file> <dest_file>")
+    exit(1)
+}
+
+let source = arguments[1]
+let dest = arguments[2]
+print("Loading \(source) into \(dest)")
+
+guard let pairs = NSDictionary(contentsOfFile: source) else {
+    print("error: Unable to load strings file: \(source)", to: &errorStream)
+    exit(1)
+}
+
+// MARK: Types
 
 // Whether the string needs multiple representations based on a numeric parameter
 enum Plurality {
@@ -19,15 +67,15 @@ enum Plurality {
     case multi
 }
 
-func == (left: Key, right: Key) -> Bool {
+func == (left : Key, right : Key) -> Bool {
     return left.path == right.path && left.name == right.name
 }
 
-struct Key: Hashable {
-    let path: [String]
-    let name: String
-    let original: String
-    
+struct Key : Hashable {
+    let path : [String]
+    let name : String
+    let original : String
+
     func hash(into hasher: inout Hasher) {
         hasher.combine(name)
         hasher.combine(path.reduce(0) {$0 ^ $1.hashValue})
@@ -35,79 +83,47 @@ struct Key: Hashable {
 }
 
 class Group {
-    let name: String
-    var children: [String:Group] = [:]
-    var items: [Item] = []
+    let name : String
+    var children : [String:Group] = [:]
+    var items : [Item] = []
     
-    init(name: String) {
+    init(name : String) {
         self.name = name
     }
 }
 
 // A localization item
 class Item {
-    let key: Key
-    var values: [String]
-    let arguments: [String]
-    let plurality: Plurality
-    let file: String
+    let key : Key
+    var values : [String]
+    let arguments : [String]
+    let plurality : Plurality
     
-    init(key: Key, values: [String], arguments: [String], plurality: Plurality, file: String) {
+    init(key : Key, values: [String], arguments: [String], plurality: Plurality) {
         self.key = key
         self.values = values
         self.arguments = arguments
         self.plurality = plurality
-        self.file = file
     }
     
     var arrayItem: Bool { return Int(key.name) != nil }
-    
-    var code: String {
-        let table = URL(string: file)?.deletingPathExtension().lastPathComponent ?? ""
-        
-        if arguments.count > 0 {
-            let args = arguments.map {
-                return "\(variableName($0)): String"
-            }.joined(separator: ", ")
-            let formatParams = arguments.map {
-                return "\"\($0)\": \(variableName($0))"
-            }.joined(separator: ", ")
-            
-            switch plurality {
-            case .single:
-                return "@objc static func \(variableName(key.name))(\(args)) -> String { return OEXLocalizedStringFromTable(\"\(key.original)\", \"\(table)\", nil).oex_format(withParameters: [\(formatParams)]) }"
-            case .multi:
-                if arguments.count == 1 {
-                    let arg = arguments[0]
-                    let name = variableName(arg)
-                    return "@objc static func \(variableName(key.name))(\(name): Int, formatted: String? = nil) -> String { return OEXLocalizedStringPluralFromTable(\"\(key.name)\", \"\(table)\", \(name), nil).oex_format(withParameters: [\"\(arg)\": formatted ?? \(name).description]) }"
-                } else {
-                    return "@objc static func \(variableName(key.name))(\(args)) -> ((Int) -> String) { return { pluralizingCount in OEXLocalizedStringPluralFromTable(\"\(key.name)\", \"\(table)\", pluralizingCount, nil).oex_format(withParameters: [\(formatParams)]) }}"
-                }
-            }
-        } else {
-            switch plurality {
-            case .single:
-                return "@objc static var \(variableName(key.name)) = OEXLocalizedStringFromTable(\"\(key.original)\", \"\(table)\", nil)"
-            case .multi:
-                return "@objc static func \(variableName(key.name))(count: Int) -> String { return OEXLocalizedStringPluralFromTable(\"\(key.name)\",  \"\(table)\", count, nil) }"
-            }
-        }
-    }
 }
 
-enum ArgumentError: Error {
+enum ArgumentError : Error {
     case unterminated
 }
 
-func symbolName(_ string: String, leadingCapital: Bool) -> String {
+// MARK: Formatting
+
+func symbolName(_ string : String, leadingCapital: Bool) -> String {
     let parts = string.components(separatedBy: "_")
-    var formattedParts: [String] = []
+    var formattedParts : [String] = []
     var first = true
     for part in parts {
         if first && !leadingCapital {
             formattedParts.append(part.lowercased())
-        } else {
+        }
+        else {
             formattedParts.append(part.capitalized)
         }
         first = false
@@ -115,28 +131,69 @@ func symbolName(_ string: String, leadingCapital: Bool) -> String {
     return formattedParts.joined(separator: "")
 }
 
-func variableName(_ string: String) -> String {
+func variableName(_ string : String) -> String {
     return symbolName(string, leadingCapital: false)
 }
 
-func className(_ string: String) -> String {
+func className(_ string : String) -> String {
     return symbolName(string, leadingCapital: true)
 }
 
-func getArguments(_ string: String) throws -> [String] {
-    var arguments: [String] = []
+extension Item {
+    
+    var code : String {
+        if arguments.count > 0 {
+            let args = arguments.map {
+                return "\(variableName($0)) : String"
+                }.joined(separator: ", ")
+            let formatParams = arguments.map {
+                return "\"\($0)\" : \(variableName($0))"
+                }.joined(separator: ", ")
+            
+            switch plurality {
+            case .single:
+                return "@objc static func \(variableName(key.name))(\(args)) -> String { return OEXLocalizedString(\"\(key.original)\", nil).oex_format(withParameters: [\(formatParams)]) }"
+            case .multi:
+                if arguments.count == 1 {
+                    let arg = arguments[0]
+                    let name = variableName(arg)
+                    return "@objc static func \(variableName(key.name))(\(name) : Int, formatted : String? = nil) -> String { return OEXLocalizedStringPlural(\"\(key.name)\", \(name), nil).oex_format(withParameters: [\"\(arg)\": formatted ?? \(name).description]) }"
+                    
+                }
+                else {
+                    return "@objc static func \(variableName(key.name))(\(args)) -> ((Int) -> String) { return {pluralizingCount in OEXLocalizedStringPlural(\"\(key.name)\", pluralizingCount, nil).oex_format(withParameters: [\(formatParams)]) }}"
+                }
+            }
+        }
+        else {
+            switch plurality {
+            case .single:
+                return "@objc static var \(variableName(key.name)) = OEXLocalizedString(\"\(key.original)\", nil)"
+            case .multi:
+                return "@objc static func \(variableName(key.name))(count : Int) -> String { return OEXLocalizedStringPlural(\"\(key.name)\", count, nil) } "
+            }
+        }
+    }
+}
+
+func getArguments(_ string : String) throws -> [String] {
+    var arguments : [String] = []
     var current = string.startIndex
     
     while current < string.endIndex {
         if let start = string.range(of: "{", options:NSString.CompareOptions(), range:current ..< string.endIndex) {
             if let end = string.range(of: "}", options:NSString.CompareOptions(), range:start.lowerBound ..< string.endIndex) {
+                
                 let argument = string[string.index(after: start.lowerBound)..<string.index(before: end.upperBound)]
+                
                 arguments.append(String(argument))
                 current = end.upperBound
-            } else {
+            }
+            else {
                 throw ArgumentError.unterminated
             }
-        } else {
+        }
+        else {
             break
         }
     }
@@ -146,59 +203,30 @@ func getArguments(_ string: String) throws -> [String] {
 
 /// Takes a string key of the form A.B.C##count and returns a parsed Key and the plurality
 /// For example, FOO.BAR.BAZ##other would return (Key(path:[FOO, BAR], name:BAZ), .Multi)
-func parseKey(_ key: String) -> (Key, Plurality) {
+func parseKey(_ key : String) -> (Key, Plurality) {
     let components = key.components(separatedBy: ".")
     let path = Array(components[0 ..< components.count - 1])
     
     guard let base = components.last else {
-        print("error: Invalid Key \(key)", to: &errorStream)
+        print("error: Invalid Key \(key)", to:&errorStream)
         exit(1)
     }
     let parts = base.components(separatedBy: "##")
     if parts.count > 1 {
-        return (Key(path: path, name: parts[0], original: key), .multi)
-    } else {
-        return (Key(path:path, name: base, original: key), .single)
+        return (Key(path: path, name: parts[0], original:key), .multi)
+    }
+    else {
+        return (Key(path:path, name: base, original:key), .single)
     }
 }
 
-let arguments = CommandLine.arguments
-guard arguments.count > 2 else {
-    print("error: Not enough arguments. Aborting")
-    print("Usage: ")
-    print("Localizations.swift <source_directory> <destination_file>")
-    exit(1)
-}
-let source = arguments[1]
-let destination = arguments[2]
-
-let url = URL(fileURLWithPath: destination)
-try? "".write(to: url, atomically: false, encoding: .utf8)
-
-let fileUpdater = try? FileHandle(forUpdating: url)
-
-func writeToFile(_ text: String) {
-    let formattedText = text + "\n"
-    // Function which when called will cause all updates to start from end of the file
-    fileUpdater?.seekToEndOfFile()
-    // Which lets the caller move editing to any position within the file by supplying an offset
-    fileUpdater?.write(formattedText.data(using: .utf8)!)
-    // Once we convert our new content to data and write it, we close the file and that’s it!
-}
-
-func readFileContentsAndVerify(_ path: String) -> NSDictionary {
-    guard let pairs = NSDictionary(contentsOfFile: source) else {
-        print("error: Could not read file \(path)", to:&errorStream)
-        exit(1)
-    }
-    return pairs
-}
+//MARK: Process Items
 
 // Check that all items have a comment
-func verifyItems(_ items: [Item], _ file: String) -> Bool {
+func verifyItems(_ items : [Item]) {
     var foundError = false
-    guard let content = try? NSString(contentsOfFile: file, encoding: String.Encoding.utf8.rawValue) else {
-        print("error: Could not open file \(file)", to: &errorStream)
+    guard let content = try? NSString(contentsOfFile: source, encoding: String.Encoding.utf8.rawValue) else {
+        print("error: Could not open file \(source).", to: &errorStream)
         exit(1)
     }
     for item in items {
@@ -216,137 +244,112 @@ func verifyItems(_ items: [Item], _ file: String) -> Bool {
         }
     }
     if foundError {
-        print("error: Some error occured while parsing file: \(file)", to: &errorStream)
         exit(1)
-    } else {
-        return true
     }
 }
 
-func verifyPairs(_ pairs: NSDictionary, _ file: String) {
-    var items: [Key : Item] = [:]
+// First pass get the items and verify they're okay
+var items : [Key : Item] = [:]
+for pair in pairs {
+    let stringKey = pair.key as! String
+    let value = pair.value as! String
     
-    for pair in pairs {
-        let stringKey = pair.key as! String
-        let value = pair.value as! String
-        
-        let (key, plurality) = parseKey(stringKey)
-        
-        do {
-            let arguments = try getArguments(value)
-            if let existing = items[key] {
-                if Set(existing.arguments) != Set(arguments) {
-                    print("error: Plural cases have different arguments. Key: \(key.name). Arguments: \(existing.arguments) & \(arguments)", to: &errorStream)
-                    exit(1)
-                }
-                existing.values.append(value)
-            } else {
-                let item = Item(key: key, values: [value], arguments: arguments, plurality: plurality, file: file)
-                items[key] = item
+    let (key, plurality) = parseKey(stringKey)
+    
+    do {
+        let arguments = try getArguments(value)
+        if let existing = items[key] {
+            if Set(existing.arguments) != Set(arguments) {
+                print("error: Plural cases have different arguments. Key: \(key.name). Arguments: \(existing.arguments) & \(arguments)", to: &errorStream)
+                exit(1)
             }
-        } catch {
-            print("error: Invalid string \(value)", to: &errorStream)
-            exit(1)
+            existing.values.append(value)
+        }
+        else {
+            let item = Item(key: key, values: [value], arguments: arguments, plurality: plurality)
+            items[key] = item
         }
     }
-    
-    if verifyItems(Array(items.values), file) {
-        for item in items.values {
-            var currentGroup = topLevel
-            for segment in item.key.path {
-                if let child = currentGroup.children[segment] {
-                    currentGroup = child
-                } else {
-                    let child = Group(name:segment)
-                    currentGroup.children[segment] = child
-                    currentGroup = child
-                }
-            }
-            currentGroup.items.append(item)
+    catch {
+        print("error: Invalid string \(value)", to: &errorStream)
+        exit(1)
+    }
+}
+
+verifyItems(Array(items.values))
+
+// Second pass. Group them by path so we get nested structures
+var topLevel = Group(name:"Strings")
+for item in items.values {
+    var currentGroup = topLevel
+    for segment in item.key.path {
+        if let child = currentGroup.children[segment] {
+            currentGroup = child
         }
-    } else {
-        print("error: Unable to verify Items", to: &errorStream)
-        exit(1)
+        else {
+            let child = Group(name:segment)
+            currentGroup.children[segment] = child
+            currentGroup = child
+        }
     }
+    currentGroup.items.append(item)
 }
 
-func readDictionary(_ file: String) {
-    guard let pairs = NSDictionary(contentsOfFile: file) else {
-        print("error: Could not read file \(file)", to: &errorStream)
-        exit(1)
-    }
-    
-    verifyPairs(pairs, file)
-}
+//MARK: Output
 
-func tabs(_ number: UInt) -> String {
-    var result: String = ""
+func tabs(_ number : UInt) -> String {
+    var result : String = ""
     for _ in 0..<number {
         result += "\t"
     }
     return result
 }
 
-func printArray(_ group: Group, indent: String) {
+guard var output = FileOutputStream(path:dest) else {
+    print("error: Couldn't open file \(dest) for writing", to: &errorStream)
+    exit(1)
+}
+
+func printArray(_ group : Group, indent: String) {
     var values = [String]()
     let sortedItems = group.items.sorted { return $0.key.name.compare($1.key.name) == .orderedAscending }
     for item in sortedItems {
-        let table = URL(string: source)?.deletingPathExtension().lastPathComponent ?? ""
-        values.append("OEXLocalizedStringFromTable(\"\(item.key.original)\", \"\(table)\", nil)")
-        writeToFile("")
+        values.append("OEXLocalizedString(\"\(item.key.original)\", nil)")
     }
-    writeToFile("\(indent)static var \(variableName(group.name)) = [\(values.joined(separator: ", "))]")
-    writeToFile("")
+    print("\(indent)static var \(variableName(group.name)) = [\(values.joined(separator: ", "))]", to: &output)
 }
 
-func printGroup(_ group: Group, depth: UInt = 0) {
+func printGroup(_ group : Group, depth : UInt = 0) {
     let indent = tabs(depth)
     guard !(group.items.first?.arrayItem ?? false) else {
         printArray(group, indent: indent)
         return
     }
     
-    writeToFile("\(indent)@objc class \(className(group.name)): NSObject {")
-    writeToFile("")
+    print("\(indent)@objc class \(className(group.name)) : NSObject {", to: &output)
     if group.children.count > 0 {
+        print("", to: &output)
         for name in group.children.keys.sorted() {
             let child = group.children[name]!
             printGroup(child, depth: depth + 1)
         }
+        print("", to: &output)
     }
     
     if group.items.count > 0 {
         let childIndent = tabs(depth + 1)
+        print("", to: &output)
         for item in (group.items.sorted {$0.key.name < $1.key.name}) {
-            writeToFile("\(childIndent)\(item.code)")
+            print("\(childIndent)\(item.code)", to: &output)
         }
+        print("", to: &output)
     }
-    writeToFile("\(indent)}")
-    writeToFile("")
+    print("\(indent)}", to: &output)
 }
 
-// list all files in directory
-func prepareItemsFromFiles(_ path: String)  {
-    var results = [String]()
-    let fileManager = FileManager.default
-    guard let enumerator = fileManager.enumerator(atPath: path) else {
-        print("error: unable to enumerate file at: \(path)", to: &errorStream)
-        exit(1)
-    }
-    for file in enumerator.allObjects as! [String] {
-        if !file.hasPrefix("InfoPlist") && file.hasSuffix(".strings") {
-            results.append(file)
-        }
-    }
-    
-    for result in results {
-        let file = "\(path)/\(result)"
-        readDictionary(file)
-    }
-}
+print("// This file is autogenerated. Do not modify.", to: &output)
+print("\n", to: &output)
 
-prepareItemsFromFiles(source)
-writeToFile("// This file is autogenerated. Do not modify. \n")
 printGroup(topLevel)
 
-fileUpdater?.closeFile()
+print("", to: &output)
