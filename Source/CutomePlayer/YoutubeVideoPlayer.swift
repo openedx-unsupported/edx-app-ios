105aeb8f2e157977259d26b13f086b915d5d20de
Muhammad Umer <mumer92@gmail.com>
2022-02-14 14:35:31 +0500

    fix: fix youtube video orientation issue

--- Source/CutomePlayer/YoutubeVideoPlayer.swift
+++ Source/CutomePlayer/YoutubeVideoPlayer.swift
@@ -177,6 +177,13 @@ extension YoutubeVideoPlayer: WKYTPlayerViewDelegate {
 
     func playerViewDidBecomeReady(_ playerView: WKYTPlayerView) {
         // call play video when the player is finished loading.
+        
+        let orientation: Bool
+        if #available(iOS 13.0, *) {
+            orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation.isPortrait ?? false
+        } else {
+            orientation = UIApplication.shared.statusBarOrientation.isPortrait
+        }
         setVideoPlayerMode(isPortrait: UIDevice.current.orientation.isPortrait)
         loadingIndicatorView.stopAnimating()
         playerView.playVideo()
