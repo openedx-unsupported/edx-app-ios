f1f6cd70d3061c70a4378ff47827641a627df4e3
Saeed Bashir <saeed.aik@gmail.com>
2022-02-16 14:09:44 +0500

    feat: handle Get Support upon error during IAP

--- Source/CourseUpgradeCompletion.swift
+++ Source/CourseUpgradeCompletion.swift
@@ -7,10 +7,11 @@
 //
 
 import Foundation
+import MessageUI
 
 let CourseUpgradeCompletionNotification = "CourseUpgradeCompletionNotification"
 
-class CourseUpgradeCompletion {
+class CourseUpgradeCompletion: NSObject {
     
     static let courseID = "CourseID"
     static let blockID = "BlockID"
@@ -23,7 +24,7 @@ class CourseUpgradeCompletion {
         case error
     }
     
-    private init() { }
+    private override init() { }
     
     func handleCourseUpgrade(state: CompletionState, screen: CourseUpgradeScreen) {
         switch state {
@@ -50,11 +51,39 @@ class CourseUpgradeCompletion {
     func showError() {
         guard let topController = UIApplication.shared.topMostController() else { return }
         let alertController = UIAlertController().showAlert(withTitle: Strings.CourseUpgrade.failureAlertTitle, message: Strings.CourseUpgrade.failureAlertMessage, cancelButtonTitle: nil, onViewController: topController) { _, _, _ in }
-        alertController.addButton(withTitle: Strings.CourseUpgrade.failureAlertGetHelp) { action in
-            
+        alertController.addButton(withTitle: Strings.CourseUpgrade.failureAlertGetHelp) { [weak self] action in
+            self?.launchEmailComposer()
         }
         alertController.addButton(withTitle: Strings.close, style: .default) { action in
             
         }
+    }
+}
+
+extension CourseUpgradeCompletion: MFMailComposeViewControllerDelegate {
+    fileprivate func launchEmailComposer() {
+        guard let controller = UIApplication.shared.window?.rootViewController else { return }
+
+        if !MFMailComposeViewController.canSendMail() {
+            UIAlertController().showAlert(withTitle: Strings.emailAccountNotSetUpTitle, message: Strings.emailAccountNotSetUpMessage, onViewController: controller)
+        } else {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.navigationBar.tintColor = OEXStyles.shared().navigationItemTintColor()
+            mail.setSubject(Strings.CourseUpgrade.getSupportEmailSubject)
+
+            mail.setMessageBody(EmailTemplates.supportEmailMessageTemplate(), isHTML: false)
+            if let fbAddress = OEXRouter.shared().environment.config.feedbackEmailAddress() {
+                mail.setToRecipients([fbAddress])
+            }
+            controller.present(mail, animated: true, completion: nil)
+
+        }
+    }
+
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        guard let controller = UIApplication.shared.window?.rootViewController else { return }
+
+        controller.dismiss(animated: true, completion: nil)
     }
 }
