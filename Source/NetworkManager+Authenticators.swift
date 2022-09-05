788afca97f4e82bec1fa44ae0993102688ad2026
Muhammad Umer <mumer92@gmail.com>
2022-09-06 03:11:42 +0500

    refactor: cleanup

--- Source/NetworkManager+Authenticators.swift
+++ Source/NetworkManager+Authenticators.swift
@@ -32,17 +32,14 @@ extension NetworkManager {
         }
         
         if let data = data,
-            let response = response
-        {
+           let response = response {
             do {
                 let raw = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions())
                 let json = JSON(raw)
                 
                 guard let statusCode = OEXHTTPStatusCode(rawValue: response.statusCode),
-                    let error = NSError(json: json, code: response.statusCode), statusCode.is4xx else
-                {
-                    return .proceed
-                }
+                      let error = NSError(json: json, code: response.statusCode), statusCode.is4xx
+                else { return .proceed }
                 
                 guard let refreshToken = session.token?.refreshToken else {
                     return logout(router: router)
@@ -56,14 +53,15 @@ extension NetworkManager {
                     }
                 }
                 
-                if error.isAPIError(code: .OAuth2InvalidGrant) || error.isAPIError(code: .OAuth2DisabledUser) || error.isAPIError(code: .OAuth2Nonexistent) {
+                if error.isAPIError(code: .OAuth2InvalidGrant)
+                    || error.isAPIError(code: .OAuth2DisabledUser)
+                    || error.isAPIError(code: .OAuth2Nonexistent) {
                     return logout(router: router)
                 }
             }
-            catch  let error as NSError {
+            catch let error as NSError {
                 print("Failed to load: \(error.localizedDescription)")
             }
-            
         }
         
         Logger.logError("Network Authenticator", "Request failed: " + response.debugDescription)
@@ -75,17 +73,17 @@ private func logout(router:OEXRouter?) -> AuthenticationAction {
     DispatchQueue.main.async {
         router?.logout()
     }
-    return AuthenticationAction.proceed
+    return .proceed
 }
 
 /** Creates a networkRequest to refresh the access_token. If successful, the
  new access token is saved and a successful AuthenticationAction is returned.
  */
 private func refreshAccessToken(router: OEXRouter?, clientId: String, refreshToken: String, session: OEXSession) -> AuthenticationAction {
-    // Set token status refreshing
+    
     router?.environment.networkManager.tokenStatus = .authenticating
-
-    return AuthenticationAction.authenticate( { (networkManager,  completion) in
+    
+    return .authenticate { networkManager, completion in
         let networkRequest = LoginAPI.requestTokenWithRefreshToken(
             refreshToken: refreshToken,
             clientId: clientId,
@@ -94,23 +92,21 @@ private func refreshAccessToken(router: OEXRouter?, clientId: String, refreshTok
         
         networkManager.performTaskForRequest(networkRequest) { [weak networkManager] result in
             var success = false
+            
             if let currentUser = session.currentUser {
                 if let newAccessToken = result.data {
-                    session.save(newAccessToken, userDetails: currentUser)
                     success = true
+                    session.save(newAccessToken, userDetails: currentUser)
                     networkManager?.tokenStatus = .valid
                 } else {
                     networkManager?.tokenStatus = .expired
                 }
-                
-                // We need to call this method to allow tasks to callback thier handlers with success or error
-                networkManager?.performQueuedTasksIfAny(success: success)
+                networkManager?.performQueuedTasks(success: success)
             } else {
-                // Remove all queued tasks if user details are not available in session
                 networkManager?.removeAllQueuedTasks()
             }
             
             return completion(success)
         }
-    })
+    }
 }
