7718f36023021a4c8bea7bf65954e89d5d239cdf
Muhammad Umer <mumer92@gmail.com>
2022-12-09 17:28:08 +0500

    chore: update content cell

--- Source/NewDashboardContentCell.swift
+++ Source/NewDashboardContentCell.swift
@@ -12,30 +12,14 @@ class NewDashboardContentCell: UITableViewCell {
     static let identifier = "NewDashboardContentCell"
     
     var viewController: UIViewController?
-    
-    private lazy var label = UILabel()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)        
         selectionStyle = .none
-        setupViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupViews() {
-        contentView.addSubview(label)
-        
-        label.snp.makeConstraints { make in
-            make.edges.equalTo(contentView)
-        }
-    }
-    
-    func setTitle(title: String) {
-        label.text = title
     }
     
     override func prepareForReuse() {
