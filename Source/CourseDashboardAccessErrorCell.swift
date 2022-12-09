fd3f6dcc5d4b8f803c6346484f6d9a33c2cdd39b
Saeed Bashir <sbashir@edx.org>
2022-12-09 16:40:39 +0500

    chore: Course Dashboard Error Screen for Non-Upgradable Courses (#1708)

---
+++ Source/CourseDashboardAccessErrorCell.swift
@@ -0,0 +1,127 @@
+//
+//  CourseDashboardAccessErrorCell.swift
+//  edX
+//
+//  Created by Saeed Bashir on 12/2/22.
+//  Copyright © 2022 edX. All rights reserved.
+//
+
+import Foundation
+
+class CourseDashboardAccessErrorCell: UITableViewCell {
+    static let identifier = "CourseDashboardAccessErrorCell"
+
+    var findCourseAction: (() -> Void)?
+
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.accessibilityIdentifier = "CourseDashboardAccessErrorCell:title-label"
+        return label
+    }()
+
+    private var infoLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.accessibilityIdentifier = "CourseDashboardAccessErrorCell:info-label"
+
+        return label
+    }()
+
+    private lazy var findCourseButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.accessibilityIdentifier = "CourseDashboardAccessErrorCell:findcourse-button"
+        button.backgroundColor = OEXStyles.shared().secondaryBaseColor()
+        button.oex_addAction({ [weak self] _ in
+            self?.findCourseAction?()
+        }, for: .touchUpInside)
+
+        let style = OEXTextStyle(weight: .normal, size: .xLarge, color: OEXStyles.shared().neutralWhite())
+        button.setAttributedTitle(style.attributedString(withText: Strings.CourseDashboard.Error.findANewCourse), for: UIControl.State())
+
+        return button
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        accessibilityIdentifier = "CourseDashboardAccessErrorCell:view"
+
+        configureView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureView() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(infoLabel)
+        contentView.addSubview(findCourseButton)
+
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentView).offset(2 * StandardVerticalMargin)
+            make.leading.equalTo(contentView).offset(StandardHorizontalMargin)
+            make.trailing.equalTo(contentView).inset(StandardHorizontalMargin)
+        }
+
+        infoLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(2 * StandardVerticalMargin)
+            make.leading.equalTo(titleLabel)
+            make.trailing.equalTo(titleLabel)
+        }
+
+        findCourseButton.snp.makeConstraints { make in
+            make.top.equalTo(infoLabel.snp.bottom).offset(4 * StandardVerticalMargin)
+            make.leading.equalTo(titleLabel)
+            make.trailing.equalTo(titleLabel)
+            make.height.equalTo(StandardVerticalMargin * 5.5)
+            make.bottom.equalTo(contentView)
+        }
+    }
+
+    func setError(course: OEXCourse?) {
+        guard let course = course, let access = course.courseware_access else { return }
+
+        let title: String
+        let info: String
+
+        if course.isEndDateOld {
+            title = Strings.CourseDashboard.Error.courseEndedTitle
+            info = Strings.CourseDashboard.Error.courseAccessExpiredInfo
+        }
+        else {
+            switch access.error_code {
+            case .startDateError:
+                title = Strings.CourseDashboard.Error.courseNotStartedTitle
+                info = formatedStartDate(displayInfo: course.start_display_info)
+            case .auditExpired:
+                title = Strings.CourseDashboard.Error.courseAccessExpiredTitle
+                info = Strings.CourseDashboard.Error.courseAccessExpiredInfo
+            default:
+                title = Strings.CourseDashboard.Error.courseAccessExpiredTitle
+                info = Strings.CourseDashboard.Error.courseAccessExpiredInfo
+                break
+            }
+        }
+
+        let titleTextStyle = OEXTextStyle(weight: .bold, size: .xLarge, color: OEXStyles.shared().neutralBlackT())
+        let infoTextStyle = OEXTextStyle(weight: .normal, size: .base, color: OEXStyles.shared().neutralXDark())
+
+        titleLabel.attributedText = titleTextStyle.attributedString(withText: title)
+        infoLabel.attributedText = infoTextStyle.attributedString(withText: info)
+    }
+
+    private func formatedStartDate(displayInfo: OEXCourseStartDisplayInfo) -> String {
+        if let displayDate = displayInfo.displayDate, displayInfo.type == .string && !displayDate.isEmpty {
+            return Strings.CourseDashboard.Error.courseNotStartedInfo(startDate: displayDate)
+        }
+
+        if let displayDate = displayInfo.date as? NSDate, displayInfo.type == .timestamp {
+            let formattedDisplayDate = DateFormatting.format(asMonthDayYearString: displayDate) ?? ""
+            return Strings.CourseDashboard.Error.courseNotStartedInfo(startDate: formattedDisplayDate)
+        }
+
+        return Strings.courseNotStarted
+    }
+}
--- Source/NewCourseDashboardViewController.swift
+++ Source/NewCourseDashboardViewController.swift
@@ -22,7 +22,8 @@ class NewCourseDashboardViewController: UIViewController, InterfaceOrientationOv
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.accessibilityIdentifier = "NewCourseDashboardViewController:table-view"
-        tableView.register(NewDashboardErrorPlaceHolderCell.self, forCellReuseIdentifier: NewDashboardErrorPlaceHolderCell.identifier)
+        tableView.register(CourseDashboardErrorViewCell.self, forCellReuseIdentifier: CourseDashboardErrorViewCell.identifier)
+        tableView.register(CourseDashboardAccessErrorCell.self, forCellReuseIdentifier: CourseDashboardAccessErrorCell.identifier)
         tableView.delegate = self
         tableView.dataSource = self
         return tableView
@@ -30,7 +31,7 @@ class NewCourseDashboardViewController: UIViewController, InterfaceOrientationOv
     
     private var course: OEXCourse?
     private var error: NSError?
-    
+
     private let courseStream: BackedStream<UserCourseEnrollment>
     private let loadStateController: LoadStateViewController
     
@@ -68,10 +69,19 @@ class NewCourseDashboardViewController: UIViewController, InterfaceOrientationOv
         view.addSubview(tableView)
         
         loadStateController.setupInController(controller: self, contentView: tableView)
-        
-        tableView.snp.makeConstraints { make in
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateViewConstraints()
+    }
+
+    override func updateViewConstraints() {
+        tableView.snp.remakeConstraints { make in
             make.edges.equalTo(safeEdges)
         }
+        configureHeaderView()
+        super.updateViewConstraints()
     }
     
     private func configureHeaderView() {
@@ -112,7 +122,7 @@ class NewCourseDashboardViewController: UIViewController, InterfaceOrientationOv
     private func verifyAccess(forCourse course: OEXCourse){
         if let access = course.courseware_access, !access.has_access {
             loadStateController.state = .Loaded
-            self.error = OEXCoursewareAccessError(coursewareAccess: access, displayInfo: course.start_display_info)
+            error = OEXCoursewareAccessError(coursewareAccess: access, displayInfo: course.start_display_info)
             tableView.reloadData()
         } else {
             self.course = course
@@ -127,6 +137,43 @@ class NewCourseDashboardViewController: UIViewController, InterfaceOrientationOv
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .allButUpsideDown
     }
+
+    private func redirectToDiscovery() {
+        guard let rootController = UIApplication.shared.window?.rootViewController,
+              let tabController = rootController.children.first as? EnrolledTabBarViewController else {
+            return
+        }
+
+        var learnController: UIViewController? = nil
+        for controller in tabController.viewControllers ?? [] {
+            if let controller  = controller as? ForwardingNavigationController {
+                learnController = controller.children.first(where: {$0 is LearnContainerViewController})
+                if learnController != nil { break }
+            }
+        }
+
+        guard let coursesContainer = learnController?.children.first(where: { $0 is EnrolledCoursesViewController }) else { return }
+        environment.router?.showCourseCatalog(fromController: coursesContainer, bottomBar: nil)
+    }
+
+    var showCourseAccessError: Bool {
+        guard let course = course else { return false }
+
+        if let error = error {
+            return error is OEXCoursewareAccessError
+        }
+
+        return course.isEndDateOld
+    }
+
+    var showContentNotLoadedError: Bool {
+        // add more logic here, like check for the content etc
+        return error != nil
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        tableView.reloadData()
+    }
 }
 
 extension NewCourseDashboardViewController: UITableViewDelegate {
@@ -139,7 +186,7 @@ extension NewCourseDashboardViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if error != nil {
+        if showCourseAccessError || showContentNotLoadedError {
             return 1
         }
         
@@ -147,11 +194,23 @@ extension NewCourseDashboardViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let error = error {
-            let cell = tableView.dequeueReusableCell(withIdentifier: NewDashboardErrorPlaceHolderCell.identifier, for: indexPath) as! NewDashboardErrorPlaceHolderCell
-            cell.setError(error)
+        if showCourseAccessError {
+            let cell = tableView.dequeueReusableCell(withIdentifier: CourseDashboardAccessErrorCell.identifier, for: indexPath) as! CourseDashboardAccessErrorCell
+            cell.setError(course: course)
+            cell.findCourseAction = { [weak self] in
+                self?.dismiss(animated: true) {
+                    self?.redirectToDiscovery()
+                }
+            }
             return cell
-        } else {
+        } else if showContentNotLoadedError {
+            let cell = tableView.dequeueReusableCell(withIdentifier: CourseDashboardErrorViewCell.identifier, for: indexPath) as! CourseDashboardErrorViewCell
+            cell.myCoursesAction = { [weak self] in
+                self?.dismiss(animated: true)
+            }
+            return cell
+        }
+        else {
             return UITableViewCell()
         }
     }
