2b6d5e81f6b3f050ac599fd2100a9aca15899315
Saeed Bashir <sbashir@edx.org>
2022-12-13 12:35:32 +0500

    chore: remove warnings generated by xcode 14.1

--- Source/OEXDataParser.h
+++ Source/OEXDataParser.h
@@ -20,7 +20,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (NSString*)handoutsWithData:(NSData*)receivedData;
 
-- (OEXUserDetails*)userDetailsWithData:(NSData*)receivedData;
+- (nullable OEXUserDetails*)userDetailsWithData:(NSData*)receivedData;
 
 @end
 
