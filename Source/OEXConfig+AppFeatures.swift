eeade70c5cc51cf9ece824001c8a06be075a3273
saeedbashir <sbashir@edx.org>
2023-09-28 14:03:08 +0500

    test: disable app nav by default

--- Source/OEXConfig+AppFeatures.swift
+++ Source/OEXConfig+AppFeatures.swift
@@ -84,11 +84,11 @@ extension OEXConfig {
     }
     
     var isNewDashboardEnabled: Bool {
-        return bool(forKey: "NEW_DASHBOARD_ENABLED", defaultValue: true)
+        return bool(forKey: "NEW_DASHBOARD_ENABLED", defaultValue: false)
     }
     
     var isNewComponentNavigationEnabled: Bool {
-        return bool(forKey: "NEW_COMPONENT_NAVIGATION_ENABLED", defaultValue: true) && isNewDashboardEnabled
+        return bool(forKey: "NEW_COMPONENT_NAVIGATION_ENABLED", defaultValue: false) && isNewDashboardEnabled
     }
 }
 
